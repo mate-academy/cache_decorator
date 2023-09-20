9c26bcc04bbb54505b00d8fff07c2e379d1dd134
Alex Zhidkov <72562232+abnormaltype@users.noreply.github.com>
2023-09-20 16:40:19 +0300

    Solved

--- app/main.py
+++ app/main.py
@@ -2,5 +2,19 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    results_storage = {}
+
+    def inner_checker(*args) -> str:
+
+        key = args
+        if key in results_storage:
+            print("Getting from cache")
+            return results_storage[key]
+        else:
+            print("Calculating new result")
+            value = func(*args)
+            results_storage[key] = value
+            return value
+
+    return inner_checker
