7c72adfdf4234907e08eb18166c666b448642d04
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-03-31 18:26:42 +0300

    create cache_decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cachet = {}
+
+    def wrapper(*args) -> int | str:
+        if args in cachet:
+            print("Getting from cache")
+            return cachet.get(args)
+        else:
+            print("Calculating new result")
+            cachet[args] = func(*args)
+            return cachet[args]
+
+    return wrapper
