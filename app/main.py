1cd73ef95f142c99bf5a61f4509ed777f646c82a
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-04-04 23:24:17 +0300

    added kwargs into the cache

--- app/main.py
+++ app/main.py
@@ -6,14 +6,16 @@ def cache(func: Callable) -> Callable:
     results = {}
 
     def wrapper(*args, **kwargs) -> int:
+        # parse kwargs dict into non-hashable format
+        kwargs_tuple = tuple((key, value) for key, value in kwargs.items())
         # check is the input data is present in cache
-        if args in results.keys():
+        if (args, kwargs_tuple) in results.keys():
             print("Getting from cache")
             # return result from cache
-            return results[args]
+            return results[(args, kwargs_tuple)]
         # calculate data and adding it to cache if data is not present in cache
         res = func(*args, **kwargs)
-        results[args] = res
+        results[(args, kwargs_tuple)] = res
         print("Calculating new result")
         return res
     return wrapper
