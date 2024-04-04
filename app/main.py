c1fff1b9ab917a1b043206265e8985259b2ebe36
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-04-04 20:31:35 +0300

    function with using one return statement

--- app/main.py
+++ app/main.py
@@ -6,13 +6,10 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(kwargs.items()))
-        if key in cached_results:
-            print("Getting from cache")
-            return cached_results[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_results[key] = result
-            return result
+        print("Getting from cache" if key in cached_results
+              else "Calculating new result")
+
+        return cached_results[key] if key in cached_results \
+            else cached_results.setdefault(key, func(*args, **kwargs))
 
     return wrapper
