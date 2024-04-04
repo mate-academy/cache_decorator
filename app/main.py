1537fbde4e34c247851f087ce60702554ade9939
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-04-04 22:46:54 +0300

    changed cache storage method from list to dict

--- app/main.py
+++ app/main.py
@@ -2,18 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # cache stores in format [[data-1, result-1] ... [data-n, result-n]]
-    results = []
+    # cache stores in dict [(data-1): result-1,  ... (data-n): result-n]
+    results = {}
 
     def wrapper(*args, **kwargs) -> int:
         # check is the input data is present in cache
-        if args in [_[0] for _ in results]:
+        if args in results.keys():
             print("Getting from cache")
             # return result from cache
-            return [_[1] for _ in results if _[0] == args][0]
-        # calculate data and append it to cache if data is not present in cache
+            return results[args]
+        # calculate data and adding it to cache if data is not present in cache
         res = func(*args, **kwargs)
-        results.append([args, res])
+        results[args] = res
         print("Calculating new result")
         return res
     return wrapper
