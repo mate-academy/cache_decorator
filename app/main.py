16e298e6916f2bd7e3b457b4b6571a1ad2f3554e
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-04-04 22:24:36 +0300

    implementation of cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    # cache stores in format [[data-1, result-1] ... [data-n, result-n]]
+    results = []
+
+    def wrapper(*args, **kwargs) -> int:
+        # check is the input data is present in cache
+        if args in [_[0] for _ in results]:
+            print("Getting from cache")
+            # return result from cache
+            return [_[1] for _ in results if _[0] == args][0]
+        # calculate data and append it to cache if data is not present in cache
+        res = func(*args, **kwargs)
+        results.append([args, res])
+        print("Calculating new result")
+        return res
+    return wrapper
