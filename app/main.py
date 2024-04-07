491ffe6f4566cab6be90e5ba9919569cf17a8eda
Olena Zaritska <zaritskaos@gmail.com>
2024-04-07 17:45:23 +0300

    implement py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    completed_runs = []
+
+    def wrapper(*args, **kwargs) -> Any:
+        if completed_runs:
+            for run in completed_runs:
+                if run["arguments"] == (args, kwargs):
+                    print("Getting from cache")
+                    return run["result"]
+        new_run = {
+            "arguments": (args, kwargs),
+            "result": func(*args, **kwargs)
+        }
+        completed_runs.append(new_run)
+        print("Calculating new result")
+        return new_run["result"]
+
+    return wrapper
