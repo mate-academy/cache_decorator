c04627d57872ec6c07147c73b351333dc43135ed
Andriy <grushevoyandrey@gmail.com>
2024-04-08 18:15:20 +0300

    Create cache dicorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_box = {}   # Create dict
+
+    def wrapper(*args, **kwargs) -> None:
+        if args not in cache_box:
+            result = func(*args, **kwargs)
+
+            cache_box[args] = result          # Adding data that doesn't exist
+            print("Calculating new result")
+        else:
+            result = cache_box[args]      # We take it from the kush
+            print("Getting from cache")
+        return result
+    return wrapper
+
