fd6e52f551b9ed113588a95ea9ad6d819b9c1601
Andriy <grushevoyandrey@gmail.com>
2024-04-08 20:06:37 +0300

    АFixed the check '*argsÑ/*kwargs' oand added 'functools.wraps'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_box = {}   # Create dict
 
-    def wrapper(*args, **kwargs) -> None:
-        if args not in cache_box:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        data = args + tuple(kwargs.items())
+        if data not in cache_box:
             result = func(*args, **kwargs)
-
-            cache_box[args] = result          # Adding data that doesn't exist
+            cache_box[data] = result          # Added data that doesn't exist
             print("Calculating new result")
         else:
-            result = cache_box[args]      # We take it from the kush
+            result = cache_box[data]      # We take it from the kush
             print("Getting from cache")
         return result
     return wrapper
