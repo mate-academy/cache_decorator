4512d4617ff05f9c3dd15f85e6837270cc934dd9
ulichka <ulichka7615@gmail.com>
2024-04-09 18:46:45 +0300

    deleted **kwargs and added @wraps

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     result = {}
 
-    def wrapper(*args, **kwargs) -> int:
+    @wraps(func)
+    def wrapper(*args) -> int:
+
         if args in result:
             print("Getting from cache")
         else:
-            result[args] = func(*args, **kwargs)
+            result[args] = func(*args)
             print("Calculating new result")
         return result[args]
     return wrapper
