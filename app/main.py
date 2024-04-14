cdc76efbd54f040a5c0e37bd645f37a076ee1aaa
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-04-14 13:59:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_data = {}
+
+    def wrapper(*args) -> Any:
+        cached_result = cached_data.get(args)
+
+        if cached_result is not None:
+            print("Getting from cache")
+            return cached_result
+
+        print("Calculating new result")
+        new_result = func(*args)
+        cached_data[args] = new_result
+        return new_result
+
+    return wrapper
