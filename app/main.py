80365df779654434c19da34b21575ee59cf94adf
Alex Bul <tarasov57228@gmail.com>
2024-04-16 18:30:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    dict_cache = {}
+
+    def inner(*args) -> tuple:
+        inner_args = args
+        if inner_args in dict_cache.keys():
+            print("Getting from cache")
+            return dict_cache[inner_args]
+        print("Calculating new result")
+        dict_cache[inner_args] = func(*args)
+        return dict_cache[inner_args]
+    return inner
