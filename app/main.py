91dcbeff92cff86e73d2ee58d4c03c625356f31e
Triukhan <tryukhan.d@gmail.com>
2024-04-16 23:16:22 +0300

    triukhan solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = []
+
+    def inner(*args: Any) -> Any:
+        for item in storage:
+            if item["arguments"] == args:
+                print("Getting from cache")
+                return item["result"]
+
+        print("Calculating new result")
+        result = func(*args)
+        storage.append({
+            "arguments": args,
+            "result": result
+        })
+        return result
+
+    return inner
