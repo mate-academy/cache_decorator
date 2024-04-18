df80b8b7141a5e81c835520da4a6d04cacf78304
bogdankoziy <133056142+bogdankoziy@users.noreply.github.com>
2024-04-18 08:57:51 +0300

    Solution_3

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 from typing import Callable, Any
 
+
 def cache(func: Callable) -> Callable:
     cache1 = {}
 
     def inner(*args, **kwargs) -> Any:
-        x = (args + tuple(kwargs))
-        if x not in cache1:
+        func_input = (args + tuple(kwargs))
+        if func_input not in cache1:
             result = func(*args, **kwargs)
-            cache1.update({x: result})
+            cache1.update({func_input: result})
             print("Calculating new result")
 
             return result
         print("Getting from cache")
 
-        return cache1[x]
+        return cache1[func_input]
 
     return inner
