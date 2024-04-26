bca44542ce60f0310187265822c87efa8b8eb2fa
Triukhan <tryukhan.d@gmail.com>
2024-04-26 21:09:53 +0300

    fix comments

--- app/main.py
+++ app/main.py
@@ -7,11 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Any:
         if args in storage:
             print("Getting from cache")
-            return storage[args]
-
-        print("Calculating new result")
-        result = func(*args)
-        storage[args] = result
-        return result
-
+        else:
+            print("Calculating new result")
+            storage[args] = func(*args)
+        return storage[args]
     return inner
