250ec284788c8549dc1a86d37f09e0e9ccf9c59e
bogdankoziy <133056142+bogdankoziy@users.noreply.github.com>
2024-04-26 15:59:44 +0300

    Fixed_Cache_Dec

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache1 = {}
 
+    @functools.wraps(func)
     def inner(*args, **kwargs) -> Any:
-        func_input = (args + tuple(kwargs))
+        func_input = (args + tuple(kwargs.items()))
         if func_input not in cache1:
             result = func(*args, **kwargs)
             cache1.update({func_input: result})
