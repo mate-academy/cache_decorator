0a29849f90c835b341384d2fe73f5b19d76b978a
Yaroslav K <yaroslav.krbk@gmail.com>
2024-04-29 18:35:07 +0300

    Task completed with no errors.

--- app/main.py
+++ app/main.py
@@ -2,5 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_arguments: list = []
+    cache_results: list = []
+
+    def internal(*args, **kwargs) -> Callable:
+        argument_list: list = [args, kwargs]
+        for entry_index in range(len(cache_arguments)):
+            if argument_list == cache_arguments[entry_index]:
+                print("Getting from cache")
+                return cache_results[entry_index]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_arguments.append(argument_list)
+        cache_results.append(result)
+        return result
+    return internal
