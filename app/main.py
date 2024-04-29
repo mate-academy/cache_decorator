6c707935c01e228122cf79ff1ca4bf6aa3746811
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-04-29 21:27:22 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Callable:
         key = (args, frozenset(kwargs.items()))
         if key in cached_results:
             print("Getting from cache")
@@ -19,8 +19,8 @@ def cache(func: Callable) -> Callable:
 
 
 @cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
+def long_time_func(num_1: int, num_2: int, num_3: int) -> int:
+    return (num_1 ** num_2 ** num_3) % (num_1 * num_3)
 
 
 def long_time_func_2(n_tuple: tuple, power: int) -> int:
