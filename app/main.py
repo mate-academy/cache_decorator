5043575fc6863674256ca8bd9c42d0326f90d19b
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-05-01 20:01:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, **kwargs) -> Callable:
+        if not hasattr(func, "storage"):
+            func.storage = {}
+        key = (args, tuple(kwargs.items()))
+        if key in func.storage:
+            print("Getting from cache")
+            return func.storage[key]
+        func.storage[key] = func(*args, **kwargs)
+        print("Calculating new result")
+        return func.storage[key]
+    return inner
