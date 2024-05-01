85d538794be55d06f8d4f12a594dac6e5e093b77
Artem Yatsuta <yatsutaproject@gmail.com>
2024-05-01 14:26:41 +0200

    return after if else statement

--- app/main.py
+++ app/main.py
@@ -8,10 +8,9 @@ def cache(func: Callable) -> Callable:
         key = (*args, *kwargs.items())
         if key in cache_store:
             print("Getting from cache")
-            return cache_store[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_store[key] = result
-            return result
+            cache_store[key] = func(*args, **kwargs)
+            return cache_store[key]
+        return cache_store[key]
     return inner
