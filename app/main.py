ae65e6ee14e89317c599025a6ebef5855ff9ae31
Artem Yatsuta <yatsutaproject@gmail.com>
2024-05-01 18:05:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
+    @functools.wraps(func)
     def inner(*args, **kwargs) -> any:
         key = (*args, *kwargs.items())
         if key in cache_store:
