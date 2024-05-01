8638a0059e96dc4982b1b31dbfce663fb6cb9544
Vitalii Tsukanov <vitalii.tsunakov@icloud.com>
2024-05-01 20:26:40 +0300

    Solution5

--- app/main.py
+++ app/main.py
@@ -6,30 +6,11 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Callable:
         key = (args, frozenset(kwargs.items()))
-        if key in cached_results:
-            print("Getting from cache")
-            return cached_results[key]
-        else:
+        if key not in cached_results:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_results[key] = result
-            return result
+            cached_results[key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return cached_results[key]
 
     return wrapper
-
-
-@cache
-def long_time_func(num_1: int, num_2: int, num_3: int) -> int:
-    return (num_1 ** num_2 ** num_3) % (num_1 * num_3)
-
-
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
