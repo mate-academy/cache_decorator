c14b86ac8c10b46d29933bd4eaba169035a9d51e
Yaroslav K <yaroslav.krbk@gmail.com>
2024-05-01 23:28:20 +0300

    Internal function now accepts kwargs.

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_storage: dict = {}
 
-    def internal(*args) -> Callable:
-        current_arguments: tuple = args
+    def internal(*args, **kwargs) -> Callable:
+        current_arguments: tuple = (args, tuple(kwargs.items()))
         for cached_arguments in cache_storage.keys():
             if current_arguments == cached_arguments:
                 print("Getting from cache")
                 return cache_storage[cached_arguments]
         print("Calculating new result")
-        result = func(*args)
+        result = func(*args, **kwargs)
         cache_storage[current_arguments] = result
         return result
     return internal
