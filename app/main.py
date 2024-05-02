74a5cde77d0700c54f03e39b885a33c7c441d0d6
Vova <valdi6014@gmail.com>
2024-05-02 19:02:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
 from typing import Callable
+from typing import Union
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def check_cache(*args) -> Union[int, list]:
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+
+        print("Calculating new result")
+        cache_dict[args] = func(*args)
+        return cache_dict[args]
+
+    return check_cache
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
