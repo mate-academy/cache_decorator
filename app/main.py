21f30c903d217554c4cea806ffdee638116c710f
Vova <valdi6014@gmail.com>
2024-05-03 13:24:21 +0300

    Removed unnecessary functions

--- app/main.py
+++ app/main.py
@@ -15,13 +15,3 @@ def cache(func: Callable) -> Callable:
         return cache_dict[args]
 
     return check_cache
-
-
-@cache
-def long_time_func(value_1st: int, value_2nd: int, value_3rd: int) -> int:
-    return (value_1st ** value_2nd ** value_3rd) % (value_1st * value_3rd)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
