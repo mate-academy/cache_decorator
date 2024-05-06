8f701cdc388ca23e7dbdb6945468a0f99a6c1bf7
Dima Drozdenko <anonimchik920@gmail.com>
2024-05-06 11:25:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def inner(*args) -> Any:
+        already_saved = True
+        working_dict = cache_data
+
+        for arg in args:
+            if arg not in working_dict:
+                already_saved = False
+                working_dict[arg] = {}
+            working_dict = working_dict[arg]
+
+        if not already_saved:
+            print("Calculating new result")
+            working_dict["result"] = func(*args)
+        else:
+            print("Getting from cache")
+
+        return working_dict["result"]
+
+    return inner
