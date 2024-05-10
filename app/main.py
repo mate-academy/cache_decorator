85ab341460bc557520b504ac421a25f707a8bcb7
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-10 11:55:09 +0300

    add functools.wraps and key arguments

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_data = {}
 
-    def inner(*args) -> Any:
-        if args in cache_data:
+    @wraps(func)
+    def inner(*args, **kwargs) -> Any:
+        cache_key = (args, frozenset(kwargs))
+        if cache_key in cache_data:
             print("Getting from cache")
-            return cache_data[args]
+            return cache_data[cache_key]
         print("Calculating new result")
-        cache_data[args] = func(*args)
-        return cache_data[args]
+        cache_data[cache_key] = func(*args, **kwargs)
+        return cache_data[cache_key]
     return inner
