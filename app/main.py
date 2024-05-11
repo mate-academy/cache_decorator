c2ed3c8c8803181f3dc8d493cd2e34152f824f78
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-05-11 19:16:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    def inner(*args) -> tuple:
+        key = args
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[key] = result
+            return result
+
+    return inner
