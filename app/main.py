91cec04d762565a677b99c17b1400c01e23585c8
Maxim Kantsura <maxim@smoothr.de>
2024-05-11 23:18:33 +0300

    solved

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
+
     def inner(*args: tuple) -> int:
         if args in cache_dict:
             print("Getting from cache")
