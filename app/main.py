e9d671921544f945aad6ac207921a48c97b66508
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-05-12 12:27:11 +0300

    add kwargs and fixed "else"

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 from typing import Callable
-import functools
 
 
 def cache(func: Callable) -> Callable:
     caches = {}
 
-    @functools.wraps(func)
-    def inner(*args) -> None:
+    def inner(*args, **kwargs) -> None:
         if args in caches:
             print("Getting from cache")
-            return caches[args]
         else:
             print("Calculating new result")
-            caches[args] = res = func(*args)
-        return res
+            caches[args] = func(*args)
+        return caches[args]
 
     return inner
