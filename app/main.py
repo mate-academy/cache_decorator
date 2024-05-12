d2f1b80835847719c469dac4a05e69b81eab16f9
Maksym Protsak <maksym.protsak@gmail.com>
2024-05-12 16:35:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        if args not in results:
+            results[args] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return results[args]
+    return wrapper
