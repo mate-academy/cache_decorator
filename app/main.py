4daf65ec21a461fd0fcb297605b3dfbeace9609f
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-05-12 15:06:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
-    def inner(*args) -> tuple:
-        key = args
+    @wraps(func)
+    def inner(*args, **kwargs) -> tuple:
+        key = (args, tuple(kwargs.items()))
         if key in cache_store:
             print("Getting from cache")
             return cache_store[key]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cache_store[key] = result
             return result
 
