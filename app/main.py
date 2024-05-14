c3c175d83edace5223ffb8b31f2d1fb253ad0da9
AndrewwSS <clash2clans1one@gmail.com>
2024-05-14 15:31:26 +0300

    move cache variable to decorator function

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from typing import Callable
-_CACHE = {}
 
 
 def cache(func: Callable) -> Callable:
+    cache_storage = {}
+
     def wrapper(*args: int) -> int:
-        if func not in _CACHE:
-            _CACHE[func] = {}
-        if args not in _CACHE[func]:
-            _CACHE[func][args] = func(*args)
+        if args not in cache_storage:
+            cache_storage[args] = func(*args)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return _CACHE[func][args]
+        return cache_storage[args]
+
     return wrapper
