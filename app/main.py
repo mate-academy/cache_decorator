e231004d9e023d3c0a82120428f7189d022b10e3
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-05-14 14:03:07 +0200

    Solution v2

--- app/main.py
+++ app/main.py
@@ -2,32 +2,13 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    arguments = {}
+    results_cache = {}
 
     def wrapper(*args) -> Callable:
-        if args not in arguments and isinstance(args, (int, float, tuple)):
-            result = func(*args)
-            arguments.update({args: result})
+        if args not in results_cache:
+            results_cache[args] = func(*args)
             print("Calculating new result")
-            return result
-        print("Getting from cache")
-        return arguments[args]
+        else:
+            print("Getting from cache")
+        return results_cache[args]
     return wrapper
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
