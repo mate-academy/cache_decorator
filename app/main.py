f2f1cc98a989a5fd0681bb2e903f86db9689e7ab
necobra <necobrane105@gmail.com>
2024-05-14 11:36:36 +0300

    Solution v2

--- app/main.py
+++ app/main.py
@@ -4,22 +4,13 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def inner(*args) -> Any:
-        is_already_saved = True
-        working_dict = cached_data
-
-        for value in args:
-            if value not in working_dict:
-                is_already_saved = False
-                working_dict[value] = {}
-            working_dict = working_dict[value]
-
-        if not is_already_saved:
-            print("Calculating new result")
-            working_dict["result"] = func(*args)
-        else:
+    def inner(*args: Any) -> Any:
+        if args in cached_data:
             print("Getting from cache")
+        else:
+            cached_data[args] = func(*args)
+            print("Calculating new result")
 
-        return working_dict["result"]
+        return cached_data[args]
 
     return inner
