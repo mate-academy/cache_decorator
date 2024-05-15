d32aa06795c13f37c146a98ba137c7618fd592bc
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2024-05-15 20:45:06 +0300

    1) added functools.wraps 2) saving kwargs was implemented

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     result_cache = {}
 
+    @functools.wraps(func)
     def inner(*args, **kwargs) -> Any:
-        if args in result_cache:
+        kwargs_str = str(kwargs)
+        if (args, kwargs_str) in result_cache:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            result_cache[args] = func(*args, **kwargs)
-        return result_cache[args]
+            result_cache[(args, kwargs_str)] = func(*args, **kwargs)
+        return result_cache[(args, kwargs_str)]
 
     return inner
