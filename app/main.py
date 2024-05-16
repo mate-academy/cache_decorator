f8b4c87b933761c44fe49b3b65bd86347fd447bd
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-05-16 18:52:59 +0300

    changed cache_results and type hints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache_results = dict()
+    cache_results = {}
 
-    def inner(*args) -> int:
+    def inner(*args: Any) -> Any:
         if args not in cache_results:
             print("Calculating new result")
             result = func(*args)
