4628254ee880b8ef2b6b7368ad96a7d7f97f8bd5
Alex <0634771737@ukr.net>
2024-05-17 18:21:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    memory = dict()
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (*args, *kwargs.items())
+        if key in memory:
+            print("Getting from cache")
+            return memory[key]
+        else:
+            memory_new = func(*args, **kwargs)
+            memory[key] = memory_new
+            print("Calculating new result")
+            return memory_new
+
+    return wrapper
