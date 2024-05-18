5865329e103e768b4cd44de8b0d0e51a980d58c4
haldaniko <haldaniko@gmail.com>
2024-05-18 20:15:58 +0200

    fix *args **kwargs; add @functools.wraps

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 from typing import Callable, Dict, Tuple
+import functools
 
 
 def cache(func: Callable) -> Callable:
     func_cache: Dict[Tuple, any] = {}
 
-    def wrapper(*args) -> Callable:
-        if args in func_cache:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+        if key in func_cache:
             print("Getting from cache")
-            return func_cache[args]
+            return func_cache[key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            func_cache[args] = result
+            result = func(*args, **kwargs)
+            func_cache[key] = result
             return result
 
     return wrapper
