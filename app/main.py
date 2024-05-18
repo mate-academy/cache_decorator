e10e0da0e79a4e704415b4214d2099011cb31e81
Alex <0634771737@ukr.net>
2024-05-18 15:57:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     memory = dict()
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         key = (*args, *kwargs.items())
         if key in memory:
             print("Getting from cache")
-            return memory[key]
+            result = memory[key]
         else:
             memory_new = func(*args, **kwargs)
             memory[key] = memory_new
             print("Calculating new result")
-            return memory_new
+            result = memory_new
+        return result
 
     return wrapper
