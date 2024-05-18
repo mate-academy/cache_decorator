601e2246ae2ef09e9ab4c058425080f72dc192ca
Vasyl Rysan <Widbmak@gmail.com>
2024-05-18 15:50:06 +0300

    Test solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results_storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        if args in results_storage:
+            print("Getting from cache")
+            return results_storage.get(args)
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        results_storage[args] = result
+        return results_storage.get(args)
+    return wrapper
