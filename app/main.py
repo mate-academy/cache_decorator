d29a760b262e06a05fb213de0ad05c63aceeca33
Alex <54539kalashnikov@gmail.com>
2024-05-19 15:20:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from typing import Callable
+from typing import Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    seve_res = {}
+
+    def wrapper(*args) -> Any:
+        if args in seve_res:
+            print("Getting from cache")
+            return seve_res[args]
+        else:
+            res = func(*args)
+            seve_res[args] = res
+            print("Calculating new result")
+            return res
+    return wrapper
