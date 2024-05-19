5b0c2dbf239b9c97c7f3c7f44d2810347918b914
Alex <54539kalashnikov@gmail.com>
2024-05-19 21:02:13 +0300

    Use wraps and kwargs

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from typing import Callable
-from typing import Any
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     seve_res = {}
 
-    def wrapper(*args) -> Any:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
         if args in seve_res:
             print("Getting from cache")
             res = seve_res[args]
         else:
-            res = func(*args)
+            res = func(*args, **kwargs)
             seve_res[args] = res
             print("Calculating new result")
         return res
