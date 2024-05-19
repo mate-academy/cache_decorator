abb7a17999610b8475d49004d7489cb9dbacd0a6
Dima <dima2000yan@gmail.com>
2024-05-19 22:14:40 +0300

    cache-decorator solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        key = f"{func.__name__} {str(args)} {str(kwargs)}"
+        if cached.get(key) is None:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached[key] = result
+            return result
+        print("Getting from cache")
+        return cached[key]
+    return inner
