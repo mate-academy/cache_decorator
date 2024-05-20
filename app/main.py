b81e1842a9c2dea24ea0c56aec34cb0bebcaf504
Sergii Musiienko <vpnmus@ukr.net>
2024-05-20 12:45:30 +0300

    most optimise Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     my_cache = {}
 
-    def wrapper(*args: list, **kwargs: dict) -> None:
-        dict_key = (*args, tuple(kwargs.items()))
-
-        if (*args, tuple(kwargs.items())) in my_cache:
+    def wrapper(*args: Any) -> Any:
+        if args in my_cache:
             print("Getting from cache")
         else:
-            my_cache[dict_key] = func(*args, **kwargs)
+            my_cache[args] = func(*args)
             print("Calculating new result")
-        return my_cache[dict_key]
+        return my_cache[args]
 
     return wrapper
