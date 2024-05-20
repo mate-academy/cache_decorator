dcb0d486f7615b9067292356f84b7061321fd5fb
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-05-20 14:43:51 +0300

    py-cache-decorator v2

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     stored_dict = {}
 
-    def wrapper(*args, **kwargs) -> int | list:
+    def wrapper(*args: int | tuple, **kwargs: int | tuple) -> int | list[int]:
         if args in stored_dict:
             print("Getting from cache")
-            return stored_dict[args]
+            result = stored_dict[args]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
             stored_dict[args] = result
-            return result
+        return result
     return wrapper
