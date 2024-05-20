dc624fa56e8e106f40dd518e7441323e8706feba
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-05-20 09:02:18 +0300

    2 return fix

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args: Any) -> Any:
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
         else:
             print("Calculating new result")
             cache_dict[args] = func(*args)
-            return cache_dict[args]
+        return cache_dict[args]
     return inner
