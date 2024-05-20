5df1c0e1b2841b7fefe3baab41db2fec7cc2da7f
Viktor Sveda <vinix.ru@gmail.com>
2024-05-20 13:00:25 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
-from typing import Callable, Any
+from typing import Callable, Any, Iterable
 
 
 def cache(func: Callable) -> Callable:
     cache_ = {}
 
-    def inner(*args: tuple) -> Any:
+    def inner(*args: Any) -> Any:
         if args in cache_:
             print("Getting from cache")
-            result = cache_[args]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_[args] = result
-        return result
+            cache_[args] = func(*args)
+        return cache_[args]
     return inner
