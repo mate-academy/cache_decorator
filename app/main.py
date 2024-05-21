cf3fc7eb0b1d8ab08bf681751f8413d4ec9e432d
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-05-21 12:22:11 +0300

    Added and changed type anotation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_dict = {}
 
-    def wrapper(*args, **kwargs) -> None:
+    def wrapper(*args: Any) -> Any:
         if args in cached_dict:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            cached_dict[args] = func(*args, **kwargs)
+            cached_dict[args] = func(*args)
         return cached_dict[args]
     return wrapper
