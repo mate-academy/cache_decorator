a1990e23d042c19facf5ea3505e4a939ae664894
andreykolomiec <akolomiec982@gmail.com>
2024-05-24 11:50:01 +0300

    return flake8

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def cache(func: Callable) -> Callable:
         cache_key = (args, frozenset(kwargs.items()))
 
         if cache_key in cache_store:
-            print('Getting from cache')
+            print("Getting from cache")
             return cache_store[cache_key]
         else:
-            print('Calculating new result')
+            print("Calculating new result")
             result = func(*args, **kwargs)
             cache_store[cache_key] = result
             return result
