cf55e1242da35f732334e89a43985c13269488c8
mykyta <nikita.fedotov222@gmail.com>
2024-05-25 17:06:21 +0300

    "Implemented"

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ def cache(func: Callable) -> Callable:
     result_dict = {}
 
     def wrapper(*args, **kwargs) -> Any:
-            if args in result_dict:
-                print("Getting from cache")
-                return result_dict[args]
-            else:
-                print("Calculating new result")
-                result = func(*args)
-                result_dict[args] = result
-                return result
+        if args in result_dict:
+            print("Getting from cache")
+            return result_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            result_dict[args] = result
+            return result
     return wrapper
