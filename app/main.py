afd64f1f2d61778edbc7d36ebfbabbb4464506df
mykyta <nikita.fedotov222@gmail.com>
2024-05-25 17:04:38 +0300

    "Implemented"

--- app/main.py
+++ app/main.py
@@ -3,19 +3,15 @@ from typing import Any
 
 
 def cache(func: Callable) -> Callable:
-    result_list = []
+    result_dict = {}
 
-    def wrapper(*args) -> Any:
-        if all(isinstance(arg, (int, float, tuple, bool)) for arg in args):
-            args_tuple = tuple(args)
-            if args_tuple not in result_list:
+    def wrapper(*args, **kwargs) -> Any:
+            if args in result_dict:
+                print("Getting from cache")
+                return result_dict[args]
+            else:
                 print("Calculating new result")
                 result = func(*args)
-                result_list.extend([args_tuple, result])
+                result_dict[args] = result
                 return result
-            else:
-                print("Getting from cache")
-                return result_list[result_list.index(args_tuple) + 1]
-        else:
-            return func(*args)
     return wrapper
