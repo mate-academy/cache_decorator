c6c3c3599f8e4efa2d6ca57f5d038b67cb1c489b
mykyta <nikita.fedotov222@gmail.com>
2024-05-25 18:38:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,12 +6,13 @@ def cache(func: Callable) -> Callable:
     result_dict = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        if args in result_dict:
+        key = args, frozenset(kwargs)
+        if key in result_dict:
             print("Getting from cache")
-            return result_dict[args]
+            return result_dict[key]
         else:
             print("Calculating new result")
             result = func(*args)
-            result_dict[args] = result
+            result_dict[key] = result
             return result
     return wrapper
