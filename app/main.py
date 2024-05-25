d711f4b09d0ca2c6466fbcee118389dfe3fe2a6b
mykyta <nikita.fedotov222@gmail.com>
2024-05-25 15:17:59 +0300

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from typing import Callable
+from typing import Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_list = []
+
+    def wrapper(*args) -> Any:
+        if all(isinstance(arg, (int, float, tuple, bool)) for arg in args):
+            args_tuple = tuple(args)
+            if args_tuple not in result_list:
+                print("Calculating new result")
+                result = func(args)
+                result_list.extend([args_tuple, result])
+                return result
+            else:
+                print("Getting from cache")
+                return result_list[result_list.index(args_tuple) + 1]
+    return wrapper
