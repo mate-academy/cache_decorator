16e938f60a327d514ea80ded8103b78871b9584a
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-05-26 18:46:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,13 +6,10 @@ def cache(func: Callable) -> Callable:
     all_cache = {}
 
     def inner(*args) -> int:
-        all_args = []
-        for arg in args:
-            all_args.append(arg)
-        if str(all_args) in all_cache:
+        if args in all_cache:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            all_cache[str(all_args)] = func(*args)
-        return all_cache[str(all_args)]
+            all_cache[args] = func(*args)
+        return all_cache[args]
     return inner
