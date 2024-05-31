5e78496128b6505e58d516a2b1e331fe1b420b4d
Sasha <sayonaraboy965123@gmail.com>
2024-05-31 06:08:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    @wraps(func)
+    def inner(*args, **kwargs) -> Any:
+        # Checking for Immutability
+        key = func.__name__, args, tuple(kwargs.items())
+        if any(isinstance(arg, (list, dict, set)) for arg in args) or \
+           any(isinstance(arg, (list, dict, set)) for arg in kwargs.values()):
+            raise TypeError("All arguments must be immutable")
+
+        if key in cache_storage:
+            print("Getting from cache")
+            return cache_storage[key]
+        print("Calculating new result")
+        cache_storage[key] = func(*args, **kwargs)
+        return cache_storage[key]
+
+    return inner
