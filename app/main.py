bbed647984a64d991c7c6224fa00ef91b3bfe4af
Anatoliy <an.kaminskij@gmail.com>
2024-06-05 11:57:46 +0300

    Solution upd. (used wraps(func) and only one RETURN in wrapper)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
         key = args + tuple(sorted(kwargs.items()))
         if key in cache_store:
             print("Getting from cache")
-            return cache_store[key]
+            result = cache_store[key]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
             cache_store[key] = result
-            return result
+        return result
 
-    wrapper.__name__ = func.__name__
-    wrapper.__doc__ = func.__doc__
     return wrapper
