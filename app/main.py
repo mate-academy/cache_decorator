dd52f7d6e5e74e31b9a8e23760c00680accd926c
Dmytro <dimontus02140@gmail.com>
2024-06-06 08:08:38 +0300

    solved

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cah = {}
+
+    def warp(*args) -> int:
+        if args in cah:
+            print("Getting from cache")
+            return cah[args]
+        else:
+            result = func(*args)
+            cah[args] = result
+            print("Calculating new result")
+            return result
+
+    return warp
