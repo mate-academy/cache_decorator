44d5b36b205b77295b76c4735cba225db4cec35f
Danylo Skepskyi <kol230305@gmail.com>
2024-06-07 01:39:23 +0200

    implement cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_of_results = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        keys = (args, *kwargs.items())
+
+        if func not in cache_of_results:
+            cache_of_results[func] = {}
+
+        if keys in cache_of_results[func]:
+            print("Getting from cache")
+            return cache_of_results[func][keys]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_of_results[func][keys] = result
+        return result
+    return wrapper
