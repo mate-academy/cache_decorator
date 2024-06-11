7e5aba315044a1aeab5df828d8a21b142fcde9c6
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-11 18:43:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    unique_values = {}
+
+    @wraps(func)
+    def inner(*args) -> Callable:
+        if args in unique_values:
+            print("Getting from cache")
+            return unique_values[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            unique_values[args] = result
+            return result
+    return inner
