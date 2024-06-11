ad451d80fd62566dfa4fa7ab50aff1f03b274c01
Viktoria Gera <viktoriagera123@gmail.com>
2024-06-11 22:04:30 +0300

    'Decorator'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    res = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+        if key in res:
+            print("Getting from cache")
+            return res[key]
+        else:
+            print("Calculating new result")
+            res[key] = func(*args, **kwargs)
+            return res[key]
+
+    return wrapper
