d01b4b5ac5fc53aa6691f2bcd0f28e29996fc4b4
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-12 11:10:05 +0300

    used only one return in def inner

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Callable:
         if args in unique_values:
             print("Getting from cache")
-            return unique_values[args]
         else:
             print("Calculating new result")
             unique_values[args] = func(*args)
