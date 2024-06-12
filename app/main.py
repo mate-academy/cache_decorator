09a5bbb7dab1273cb2f592005d8228e41a45ea2d
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-12 15:37:31 +0300

    added oppotunity to work with kwargs

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ def cache(func: Callable) -> Callable:
     unique_values = {}
 
     @wraps(func)
-    def inner(*args) -> Any:
-        if args in unique_values:
+    def inner(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in unique_values:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            unique_values[args] = func(*args)
-        return unique_values[args]
+            unique_values[key] = func(*args, **kwargs)
+        return unique_values[key]
     return inner
