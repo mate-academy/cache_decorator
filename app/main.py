2db3c6836937cf3cfa50f33f805cadb513133ffc
Volodymyr Purshega <vpurshega@gmail.com>
2024-06-18 22:37:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_db = {}
+
+    def wrapper(*args) -> any:
+        if args in cache_db:
+            print("Getting from cache")
+            return cache_db[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_db[args] = result
+            return result
+
+    return wrapper
