875b9ab04ae2d336daf9fe4fc4c41f4c931307cb
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-06-23 18:13:47 +0300

    Second solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache_dict = {}
+    repeat_arguments = {}
 
-    def inner(*args: tuple[int, float, str, tuple]) -> None:
-        if args in cache_dict:
+    def function_result(*args: tuple[Any]) -> Any:
+        result = func(*args)
+        if result not in repeat_arguments:
+            repeat_arguments[result] = result
             print("Getting from cache")
-            return cache_dict[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            cache_dict[args] = result
+            return result
+        print("Calculating new result")
         return result
-    return inner
+    return function_result
+
+
+@cache
+def add(a: int, b: int) -> int:
+    return a * 70 + b * 20
+
+
+@cache
+def sum_tuple(some_tuple: tuple) -> int:
+    return sum(some_tuple) * 93
+
+
+print(sum_tuple((5, 12, 34, 99, 15)))
+print(sum_tuple((5, 12, 34, 99, 15)))
+print(add(36, 48))
+print(add(36, 48))
