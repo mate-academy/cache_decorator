f2180bcc34a567e6888704a02db9ef15d055fdbc
Gariman <garimanblack1@gmail.com>
2024-06-28 11:30:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 from typing import Any, Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     results_cache = {}
 
-    def inner(*args: tuple) -> Any:
-        if args in results_cache:
+    @wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict) -> Any:
+        input_key = (args, frozenset(kwargs.items()))
+
+        if input_key in results_cache:
             print("Getting from cache")
-            return results_cache[args]
+            return results_cache[input_key]
 
         else:
             print("Calculating new result")
-            result = func(*args)
-            results_cache[args] = result
+            result = func(*args, **kwargs)
+            results_cache[input_key] = result
             return result
 
-    return inner
+    return wrapper
