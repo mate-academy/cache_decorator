e9fa0b337c2fe50477c68f958bc2667b62b25a64
Illia Dubina <armahanz.music@gmail.com>
2024-06-29 14:28:13 +0300

    add kwargs and wraps

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
 
     cache_storage = {}
 
-    def inner(*args) -> int:
+    @wraps(func)
+    def inner(*args, **kwargs) -> int:
 
         message = "Getting from cache"
-        if args not in cache_storage:
-            cache_storage[args] = func(*args)
+        params = (args, frozenset(kwargs))
+        if params not in cache_storage:
+            cache_storage[params] = func(*args, **kwargs)
             message = "Calculating new result"
 
         print(message)
-        return cache_storage[args]
+        return cache_storage[params]
 
     return inner
