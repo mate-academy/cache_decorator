70d688f1b394eeac43598f60fc399c3f450e0623
andrew_siemakin <148764306+andrewsiemakin@users.noreply.github.com>
2024-06-29 18:59:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = args + tuple(sorted(kwargs.items()))
+
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_store[key] = result
+            return result
+
+    return wrapper
