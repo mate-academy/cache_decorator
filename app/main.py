4d1367625a1e05fc545574a551d26995f8008a1e
AndreyShevchenkoDevil <ashevche20@gmail.com>
2024-07-02 21:15:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+import functools
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_memory = dict()
+    functools.wraps(func)
+
+    def wrapper(*args, **kwargs) -> Any:
+        keyword = ""
+        for arg in args:
+            keyword += str(arg)
+
+        if keyword in cache_memory.keys():
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cache_memory[keyword] = func(*args, **kwargs)
+
+        return cache_memory[keyword]
+    return wrapper
