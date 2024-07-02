28368637330fece7b3ae0483e211cfe824d31cbf
andrew_siemakin <148764306+andrewsiemakin@users.noreply.github.com>
2024-07-02 11:07:38 +0300

    Updated

--- app/main.py
+++ app/main.py
@@ -11,11 +11,9 @@ def cache(func: Callable) -> Callable:
 
         if key in cache_store:
             print("Getting from cache")
-            return cache_store[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_store[key] = result
-            return result
+            cache_store[key] = func(*args, **kwargs)
+        return cache_store[key]
 
     return wrapper
