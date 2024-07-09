02dcc6ee8f7fc94530d63a1bba639f95f412d65d
Viacheslav Miasoiedov <poznianski@gmail.com>
2024-07-09 21:59:23 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from typing import Callable
 from functools import wraps
 
+
 def cache(func: Callable) -> Callable:
     cache_dict = {}
-    
+
     @wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
         key = (func.__name__, args, frozenset(kwargs.items()))
-        
+
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
@@ -16,5 +17,5 @@ def cache(func: Callable) -> Callable:
             result = func(*args, **kwargs)
             cache_dict[key] = result
             return result
-    
+
     return wrapper
