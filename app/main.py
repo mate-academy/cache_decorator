9a9598c5814cc295e831dcb81c6bcf41fe031b55
Andrii Uvarov <aaanyyy@gmail.com>
2024-07-10 20:42:39 +0200

    flake8 fixes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from typing import Callable
 
 saved_runs = {}
+
+
 def cache(func: Callable) -> Callable:
     def arg_saver(*argv, **argc) -> callable:
-
-        arg=str(argv) + str(argc) + str(func)
+        arg = str(argv) + str(argc) + str(func)
         if arg in saved_runs:
             print("Getting from cache")
             return saved_runs[arg]
