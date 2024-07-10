cf6c75b214356ffde18a978d7d84e5c16fb840a0
Andrii Uvarov <aaanyyy@gmail.com>
2024-07-10 20:28:28 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from typing import Callable
 
-
+saved_runs = {}
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def arg_saver(*argv, **argc) -> callable:
+
+        arg=str(argv) + str(argc) + str(func)
+        if arg in saved_runs:
+            print("Getting from cache")
+            return saved_runs[arg]
+        else:
+            result = func(*argv, **argc)
+            saved_runs.update({arg: result})
+            print("Calculating new result")
+            return result
+    return arg_saver
