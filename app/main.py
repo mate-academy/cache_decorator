0b5ce58647c73bf7e9393ee5e4bd47cf02126101
Marharyta Zazulia <zazmargo@gmail.com>
2024-07-11 09:14:02 -0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable, *results) -> Callable:
+    #  'results' is list of completed functions
+    #  in 'cache_results' stores elements in the following order:
+    #  [function name, variables], result
+    cache_results = [item for item in results]
+
+    def wrapper(*args) -> Any:
+        data_of_func = [func.__name__, *args]
+        if data_of_func in cache_results:
+            index = cache_results.index(data_of_func)
+            result = cache_results[index + 1]
+            print("Getting from cache")
+        else:
+            result = func(*args)
+            cache_results.append(data_of_func)
+            cache_results.append(result)
+            print("Calculating new result")
+        return result
+    return wrapper
