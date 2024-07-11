57f08ea257a9d62258c17b18c5f3d5b458cead43
D Petetskyi <4477617@gmail.com>
2024-07-11 17:08:23 +0300

    return removed

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     def inner(*args, **kwargs) -> int:
         request = func, args + tuple(kwargs.items())
-        if request in inner.results:
-            print("Getting from cache")
-            return inner.results[request]
-        else:
+        if request not in inner.results:
             inner.results[request] = func(*args, **kwargs)
             print("Calculating new result")
+        else:
+            print("Getting from cache")
         return inner.results[request]
     inner.results = {}
 
