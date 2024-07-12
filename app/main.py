a997df084c35bb269c37dd2d0775b7f014a31f98
Marharyta Zazulia <zazmargo@gmail.com>
2024-07-12 00:09:58 -0300

    'Changed_type_of_cache_results'

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 from typing import Callable, Any
 
 
-def cache(func: Callable, *results) -> Callable:
-    #  'results' is list of completed functions
-    #  in 'cache_results' stores elements in the following order:
-    #  [function name, variables], result
-    cache_results = [item for item in results]
+def cache(func: Callable, **results) -> Callable:
+    #  "results" is a dictionary of completed functions(key) with result(value)
+    #  in dict "cache_results" stores elements in the following form:
+    #  "func(args)": result
+
+    cache_results = {key: value for key, value in results.items()}
 
     def wrapper(*args) -> Any:
-        data_of_func = [func.__name__, *args]
-        if data_of_func in cache_results:
-            index = cache_results.index(data_of_func)
-            result = cache_results[index + 1]
-            print("Getting from cache")
-        else:
+        data_of_func = f"{func.__name__}{args}"
+        if cache_results.get(data_of_func) is None:
             result = func(*args)
-            cache_results.append(data_of_func)
-            cache_results.append(result)
+            cache_results[data_of_func] = result
             print("Calculating new result")
+        else:
+            result = cache_results[data_of_func]
+            print("Getting from cache")
         return result
+
     return wrapper
