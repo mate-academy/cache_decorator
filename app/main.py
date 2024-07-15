53a099e25af5b596cfd944273296c18e4d080761
DSahalatyi <dsahalatyi@gmail.com>
2024-07-15 18:20:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_runs = {}
+
+    def wrapper(*args) -> (int, float):
+        if args not in saved_runs.keys():
+            print("Calculating new result")
+            saved_runs[args] = func(*args)
+            return saved_runs[args]
+        else:
+            print("Getting from cache")
+            return saved_runs[args]
+    return wrapper
