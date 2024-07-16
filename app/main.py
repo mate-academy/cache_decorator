973f3811c3bf9e3108221d73a80b9f3e1a110745
DSahalatyi <dsahalatyi@gmail.com>
2024-07-16 10:35:11 +0300

    fix loops order

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def cache(func: Callable) -> Callable:
     saved_runs = {}
 
     def wrapper(*args) -> (int, float):
-        if args not in saved_runs.keys():
-            print("Calculating new result")
-            saved_runs[args] = func(*args)
-            return saved_runs[args]
-        else:
+        if args in saved_runs.keys():
             print("Getting from cache")
             return saved_runs[args]
+        print("Calculating new result")
+        saved_runs[args] = func(*args)
+        return saved_runs[args]
+
     return wrapper
