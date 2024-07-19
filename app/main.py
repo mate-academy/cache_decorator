1d01ac3fff14663f69e3b71a298b9ff45371a518
Olexandr Bastov <iamlucky1990@gmail.com>
2024-07-19 09:50:34 +0300

    pull with errors (probably)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
+
+cached_data = {}
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper(*args) -> Callable:
+
+        if func.__name__ in cached_data:
+            if args in cached_data[func.__name__]:
+                print("Getting from cache")
+                return cached_data[func.__name__][args]
+            else:
+                return calc_data(*args)
+
+        cached_data[func.__name__] = {}
+        return calc_data(*args)
+
+    def calc_data(*args) -> Any:
+        print("Calculating new result")
+        new_data = func(*args)
+        cached_data[func.__name__][args] = new_data
+        return new_data
+
+    return wrapper
