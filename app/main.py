557ad3aad5bde772816c227eb8cd4b17ad63cfd3
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-07-20 16:08:16 +0300

    add cache function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+import functools
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_data = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        key = (func.__name__, args, frozenset(kwargs.items()))
+        if key in stored_data:
+            print("Getting from cache")
+            return stored_data[key]
+
+        print("Calculating new result")
+        stored_data[key] = func(*args, **kwargs)
+        return stored_data[key]
+
+    return wrapper
