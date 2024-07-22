0ec0192a8b8c1b8f0f5ca988891bbc7e1a031be1
Alexandr Politov <apolitov83@gmail.com>
2024-07-22 15:21:19 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def cache(func: Callable) -> Callable:
             return results_storage[args]
         else:
             print("Calculating new result")
-            results_storage[key] = func(*args)
-        return results_storage[key]
+            results_storage[args] = func(*args)
+        return results_storage[args]
 
     return inner_checker
