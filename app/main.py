b9e969c6473c1a57724a36433c791fec9f89f0f0
Filobokov Andrey <fibiknew@gmail.com>
2024-07-22 14:10:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    buffer = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        check = (*args, *kwargs)
+        if buffer.get((*args, *kwargs)) is None:
+            buffer[check] = func(*args, *kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return buffer[check]
+
+    return wrapper
