358c3f11fbae77047043cd71a1b7182a52538f19
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-22 22:01:56 +0300

    use functools.wraps as a decorator

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from typing import Callable
 from functools import wraps
 
+
 def cache(func: Callable) -> Callable:
 
     cache_results = {}
 
+    @wraps(func)
     def inner(*args) -> None:
 
         if args in cache_results:
