d49099711534a601dfe8dcfbf625fadda16eee93
Bogdan <bogdan.rylov422@gmail.com>
2024-07-23 23:15:15 +0300

    Remove redundant conditions to improve code efficiency

--- app/main.py
+++ app/main.py
@@ -7,21 +7,13 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(*args) -> Any:
-        is_cachable = True
-        for arg in args:
-            if isinstance(arg, (list, dict, set)):
-                is_cachable = False
-                break
-
-        if is_cachable:
-            for arguments, result in cached_results.items():
-                if arguments == args:
-                    print("Getting from cache")
-                    return result
+        for arguments, result in cached_results.items():
+            if arguments == args:
+                print("Getting from cache")
+                return result
         print("Calculating new result")
         calculated_result = func(*args)
-        if is_cachable:
-            cached_results[args] = calculated_result
+        cached_results[args] = calculated_result
         return calculated_result
 
     return wrapper
