af3c61095b2c2f0d8acefef2b399b6861065c1b9
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-07-23 18:43:41 +0300

    Task 4 fix

--- app/main.py
+++ app/main.py
@@ -9,21 +9,11 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args: Tuple[Any, ...]) -> Any:
         if args in cache_store:
             print("Getting from cache")
-            return cache_store[args]
+            result = cache_store[args]
         else:
             print("Calculating new result")
             result = func(*args)
             cache_store[args] = result
-            return result
+        return result
 
     return wrapper
-
-
-@cache
-def long_time_func(exp1: int, exp2: int, exp3: int) -> None:
-    return (exp1 ** exp2 ** exp3) % (exp1 * exp3)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> Any:
-    return [number ** power for number in n_tuple]
