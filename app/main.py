c4a8e8d77b694aeb809da489febdd8f062b456c3
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-07-23 16:02:17 +0300

    Task 4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    @wraps(func)
+    def wrapper(*args: Tuple[Any, ...]) -> Any:
+        if args in cache_store:
+            print("Getting from cache")
+            return cache_store[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[args] = result
+            return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(exp1: int, exp2: int, exp3: int) -> None:
+    return (exp1 ** exp2 ** exp3) % (exp1 * exp3)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> Any:
+    return [number ** power for number in n_tuple]
