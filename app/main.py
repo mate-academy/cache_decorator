6adf97725b37bba17f191fb80e7b1c8b10537250
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-07-31 14:00:34 +0300

    implemennt cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def wrapper(*args) -> int | tuple:
+        if args not in cache_data:
+            print("Calculating new result")
+            result = func(*args)
+            cache_data[args] = result
+            return cache_data[args]
+        else:
+            print("Getting from cache")
+            return cache_data[args]
+
+    return wrapper
