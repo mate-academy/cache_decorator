628a222d3cd4d7123780a1f702dd16e288d4745d
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-08-02 12:07:12 +0300

    add solution for task

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_cache = {}
+
+    def inner(*args) -> Callable:
+        if args in result_cache:
+            print("Getting from cache")
+            return result_cache.get(args)
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            result_cache[args] = result
+            return result
+
+    return inner
