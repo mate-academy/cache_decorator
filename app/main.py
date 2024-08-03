cba8575d307e27a4121c1446b582cd69cb588ed4
Serhii Chubur <serhichubur15@gmail.com>
2024-08-03 22:30:43 +0300

    corrected_according_to_flake8

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
+
     runs = {}
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> int:
         if args in runs:
             print("Getting from cache")
             return runs[args]
         else:
             result = func(*args, **kwargs)
             runs[args] = result
-            print("Calculating new result")
+            print("Calculating new result)
             return result
     return wrapper
