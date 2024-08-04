44df940228b07db3eb786e404406c0930f8b5e2a
Serhii Chubur <serhichubur15@gmail.com>
2024-08-04 17:43:29 +0300

    checking func name

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    runs = {}
+    name = None
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> int:
+        nonlocal name
+        arguments = (args, frozenset(kwargs.items()))
+        if arguments in runs and name == func.__name__:
+            print("Getting from cache")
+        else:
+            name = func.__name__
+            result = func(*args, **kwargs)
+            runs[arguments] = result
+            print("Calculating new result")
+        return runs[arguments]
+
+    return wrapper
