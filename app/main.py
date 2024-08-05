ef396d4b88fed1b7d43d08a46840c30f6ae74155
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-08-05 12:20:02 +0300

    fix the annotation and add kwargs support

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     result_cache = {}
 
-    def inner(*args) -> Callable:
-        if args in result_cache:
+    @wraps(func)
+    def inner(*args, **kwargs) -> Any:
+        key = (func.__name__, args, frozenset(kwargs.items()))
+
+        if key in result_cache:
             print("Getting from cache")
-            return result_cache.get(args)
+            return result_cache.get(key)
         else:
             print("Calculating new result")
-            result = func(*args)
-            result_cache[args] = result
-            return result
+            result = func(*args, **kwargs)
+            result_cache[key] = result
+
+        return result
 
     return inner
