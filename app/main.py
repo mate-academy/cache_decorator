65a62b973027410b1d4bd247a3255b75c028ee78
Serhiy <sergeynechaenko@ukr.net>
2024-08-08 22:33:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_kwargs = {}
+
+    def stored(*args, **kwargs) -> Callable:
+        checked_kwargs = tuple(kwargs.items())
+        checked = (args, checked_kwargs)
+        if checked in stored_kwargs:
+            print("Getting from cache")
+            return stored_kwargs[checked]
+        else:
+            print("Calculating new result")
+            stored_kwargs[checked] = func(*args, **kwargs)
+            return stored_kwargs[checked]
+
+    return stored
