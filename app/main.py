52227f85f2bca7eddfc596ee112258a91eff9550
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-09 18:07:12 +0300

    Use only one return in inner

--- app/main.py
+++ app/main.py
@@ -8,12 +8,13 @@ def cache(func: Callable) -> Callable:
     @wraps(func)
     def inner(*args: tuple) -> Any:
         nonlocal cache_dict
+
         if args in cache_dict.keys():
             print("Getting from cache")
-            return cache_dict[args]
         else:
             print("Calculating new result")
             cache_dict[args] = func(*args)
-            return cache_dict[args]
+
+        return cache_dict[args]
 
     return inner
