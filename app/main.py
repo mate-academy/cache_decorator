2c7f6f5f6ca0e49c87c7dfa0634cfa90ead28db2
Deadmarvald <deadmarvald@gmail.com>
2024-08-12 11:19:30 +0300

    Add support for caching with both args and kwargs

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
-    def inner(*args: list) -> Callable:
-        if args in cache_storage:
+    def inner(*args: list, **kwargs: dict) -> Callable:
+        key = (args, tuple(kwargs.items()))
+
+        if key in cache_storage:
             print("Getting from cache")
-            return cache_storage[args]
+            return cache_storage[key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_storage[args] = result
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
             return result
     return inner
