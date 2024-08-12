2dd1537d427073dbb270f3dfebd5a723bc629b5d
Kulib <kulibchenkovalera@gmail.com>
2024-08-12 17:16:14 +0300

    First commit

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result = dict()
+
+    def inner(*args) -> int:
+        if tuple(args) in result:
+            print("Getting from cache")
+            return result[tuple(args)]
+        print("Calculating new result")
+        result_of_func = func(*args)
+        result.update({tuple(args): result_of_func})
+        return result_of_func
+    return inner
