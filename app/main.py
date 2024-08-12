2a0a952bc2ed6980e20a143cfc0e4ca27e91a17a
Kulib <kulibchenkovalera@gmail.com>
2024-08-12 20:17:43 +0300

    second commit(add kwargs)

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     result = dict()
 
-    def inner(*args) -> int:
+    def inner(*args, **kwargs) -> int:
         if tuple(args) in result:
             print("Getting from cache")
             return result[tuple(args)]
         print("Calculating new result")
-        result_of_func = func(*args)
+        result_of_func = func(*args, **kwargs)
         result.update({tuple(args): result_of_func})
         return result_of_func
     return inner
