943aa7382bc6f56e753d2c9908f04f914dc52945
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-13 16:17:55 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from typing import Callable, Any
-import functools
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    @functools.wraps(func)
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         func_details = (args, tuple(kwargs.items()), func.__name__)
 
-        if func_details in cache_dict.keys():
+        if func_details in cache_dict:
             print("Getting from cache")
-            return cache_dict.get(func_details)
+            return cache_dict[func_details]
 
         print("Calculating new result")
         func_result = func(*args, **kwargs)
