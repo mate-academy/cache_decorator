9ef9742176b6bc2c60dd7a6b8c4781294d00cab3
Hennadii <bzpg545256@gmail.com>
2024-08-15 07:27:17 +0100

    functools and kwargs add

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
-    def wrapper(*args: tuple) -> Any:
-        if args in cache_store:
+    @wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict) -> Any:
+        key = (func.__name__, args, frozenset(kwargs.items()))
+
+        if key in cache_store:
             print("Getting from cache")
-            return cache_store[args]
+            return cache_store[key]
         else:
-            result = func(*args)
-            cache_store[args] = result
+            result = func(*args, **kwargs)
+            cache_store[key] = result
             print("Calculating new result")
             return result
     return wrapper
