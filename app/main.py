4bd761ad1f3473efbcd0597e99257d6dceae5f29
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-19 17:55:00 +0300

    lust fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
+    @functools.wraps(func)
     def inner(*args, **kwargs) -> Any:
         args += tuple(kwargs.values())
         if args not in cache_dict:
-            cache_dict[args] = func(*args, **kwargs)
             print("Calculating new result")
-            return cache_dict[args]
-        print("Getting from cache")
+            cache_dict[args] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
         return cache_dict[args]
     return inner
