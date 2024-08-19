d4d762403de417f6df34bdaeea4334ab7c4ba8a5
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-19 15:45:21 +0300

    args_kwargs fixed

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ def cache(func: Callable) -> Callable:
 
     def inner(*args, **kwargs) -> Any:
         if args not in cache_list:
-            cache_list[args] = func(*args, **kwargs)
+            cache_list[args or kwargs] = func(*args, **kwargs)
             print("Calculating new result")
-            return cache_list[args]
+            return cache_list[args or kwargs]
         print("Getting from cache")
         return cache_list[args]
     return inner
