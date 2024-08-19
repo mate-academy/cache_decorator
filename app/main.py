d1bf8db122c0472cd6218fb64b5f28b0c1ccdef4
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-19 13:37:40 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_list = {}
+
+    def inner(*args, **kwargs) -> Any:
+        if args not in cache_list:
+            cache_list[args] = func(*args, **kwargs)
+            print("Calculating new result")
+            return cache_list[args]
+        else:
+            print("Getting from cache")
+            return cache_list[args]
+    return inner
