9c5a7bd6f9de05de5c95aa2e868b8a3ce939804f
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-19 17:16:09 +0300

    add kwargs_alue to args

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def inner(*args) -> Any:
+    def inner(*args, **kwargs) -> Any:
+        args += tuple(kwargs.values())
         if args not in cache_dict:
-            cache_dict[args] = func(*args)
+            cache_dict[args] = func(*args, **kwargs)
             print("Calculating new result")
             return cache_dict[args]
         print("Getting from cache")
