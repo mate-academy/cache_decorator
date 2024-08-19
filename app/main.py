2df881f4809f9138eb1718803847664b500cfe05
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-08-19 20:26:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs) -> Any:
+        args += tuple(kwargs.values())
+        if args not in cache_dict:
+            print("Calculating new result")
+            cache_dict[args] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return cache_dict[args]
+    return inner
