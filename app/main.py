35a5b47a655b5ad2e320a3cb2853a8e2044b858e
yura <pollo7964132@gmail.com>
2024-08-21 19:47:22 +0300

    implemented cache Dekorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_data = {}
+
+    def inner(*args) -> Any:
+        if args not in saved_data:
+            saved_data[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return saved_data[args]
+
+    return inner
