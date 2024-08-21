215e3b5866b6fee98a54c38264f15f684b8919b9
Tetiana Sobko <sobkotiana@gmail.com>
2024-08-21 22:58:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_results = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        key = args + tuple(kwargs.values())
+
+        if key in stored_results:
+            print("Getting from cache")
+            return stored_results[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            stored_results[key] = result
+            return result
+
+    return wrapper
