ee315ce8b6e4f37a90cdf2b678f59baa374ce668
Max <mxbely98@gmail.com>
2024-08-23 19:08:17 +0300

    Created decorate function "cache" Inside this function was implemented "inner" function with save data from outside function into dictionary

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    uniques_names_functions = {}
+
+    def inner(*args) -> Any:
+        if args not in uniques_names_functions:
+            result_function = func(*args)
+            uniques_names_functions[args] = result_function
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return uniques_names_functions[args]
+    return inner
