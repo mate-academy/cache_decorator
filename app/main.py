0d6f7410cc457ca0a81403be833111d535ace3ad
Nazar Slavych <slavuch232355@gmail.com>
2024-08-23 20:46:09 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    @wraps(func)
+    def wrapper(*args) -> None:
+        key = args
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[key] = result
+            return result
+
+    return wrapper
