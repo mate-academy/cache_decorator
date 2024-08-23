f220b2fe0e3a174fb85108ce09309778fcaa5786
Lev <lev2099@gmail.com>
2024-08-23 18:02:22 +0300

    Add cache, return cache_storage[key]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
+from functools import wraps
 from typing import Callable, Any
 
 
 def cache(func: Callable) -> Any:
     cache_storage = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
 
         if key in cache_storage:
             print("Getting from cache")
-            return cache_storage[key]
         else:
             print("Calculating new result")
             cache_storage[key] = func(*args, **kwargs)
-            return cache_storage[key]
+        return cache_storage[key]
 
     return wrapper
