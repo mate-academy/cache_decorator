a909b03395347c694c7bd38149dabb3784ea12b0
pavlo <frontbastard@gmail.com>
2024-08-26 13:12:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+from functools import wraps
 from typing import Callable
+from typing import TypeVar
+
+T = TypeVar("T")
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args) -> T:
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_dict[args] = result
+            return result
+
+    return wrapper
