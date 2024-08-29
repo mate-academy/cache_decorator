27ebea386c0b705aa8eddc24172ea9a99be33737
Vadym Shevchenko <popokoa@gmail.com>
2024-08-29 17:04:49 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -6,13 +6,15 @@ def cache(func: Callable) -> Callable:
     cache_storage = {}
 
     @functools.wraps(func)
-    def wrapper(*args) -> int:
-        if not (args in cache_storage):
-            cache_storage.update({args: func(*args)})
+    def wrapper(*args, **kwargs) -> int:
+        key = (func.__name__, args, frozenset(kwargs.items()))
+
+        if key not in cache_storage:
+            cache_storage[key] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
 
-        return cache_storage.get(args)
+        return cache_storage[key]
 
     return wrapper
