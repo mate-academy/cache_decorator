83bc72820468743f0d551d7074505f6fe20f10d6
Mike Shmygel <m.shmygel@gmail.com>
2024-08-30 11:03:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    decorator_cache = {}
+
+    @functools.wraps(func)
+    def wrapper(*args) -> int:
+        if args in decorator_cache:
+            print("Getting from cache")
+            return decorator_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            decorator_cache[args] = result
+            return result
+    return wrapper
