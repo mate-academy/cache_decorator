54471ceefd39a0caea3d39ccc7fb32cec9cc548f
olgas <olgashydlovska@gmail.com>
2024-09-02 11:04:24 +0300

    Make the one return and remove tests

--- app/main.py
+++ app/main.py
@@ -10,23 +10,21 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args, **kwargs) -> any:
         # Use the arguments as the cache key
         cache_key = (func.__name__, args, tuple(sorted(kwargs.items())))
-        if cache_key in func_cache:
-            print("Getting from cache")
-            return func_cache[cache_key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
+
+        # Use a ternary operator to either return
+        # the cached value or calculate a new result
+        result = func_cache[cache_key] \
+            if cache_key in func_cache else func(*args, **kwargs)
+
+        # Print messages based on whether
+        # the result was cached or newly calculated
+        print("Getting from cache"
+              if cache_key in func_cache else "Calculating new result")
+
+        # Store the result in the cache if it was newly calculated
+        if cache_key not in func_cache:
             func_cache[cache_key] = result
-            return result
+
+        return result
 
     return wrapper
-
-
-@cache
-def long_time_func(number_1: int, number_2: int, number_3: int) -> int:
-    return (number_1 ** number_2 ** number_3) % (number_1 * number_3)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list[any]:
-    return [number ** power for number in n_tuple]
