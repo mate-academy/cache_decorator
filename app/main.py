f77cfe980da8e56e10d102190d8b7a9f5988dab2
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-09-02 09:01:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    def inner(*args: Any, **kwargs: Any) -> Any:
+        key = (func, args)
+        if key in cache_storage:
+            print("Getting from cache")
+            return cache_storage[key]
+
+        result = func(*args)
+        cache_storage[key] = result
+        print("Calculating new result")
+        return result
+    return inner
