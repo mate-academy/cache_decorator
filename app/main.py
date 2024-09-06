95110d68f519adbaa9073bcc3c9ff76cec87f64a
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2024-09-06 13:56:22 +0200

    fixed task

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ def cache(func: Callable) -> Callable:
 
         if args in cache_storage:
             print("Getting from cache")
-            return cache_storage[args]
+            result = cache_storage[args]
         else:
             print("Calculating new result")
             result = func(*args)
             cache_storage[args] = result
-            return result
+        return result
     return wrapper
