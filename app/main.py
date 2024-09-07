ee1a6b97e80ccd7405a662e513461e2f843761a0
dmytro <dmysamo@gmail.com>
2024-09-07 17:15:40 +0300

    creating cashe

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    cashe = {}
+
+    def wrapper(*args) -> Callable:
+        if args in cashe:
+            print("Getting from cache")
+            return cashe[args]
+        print("Calculating new result")
+        cashe[args] = func(*args)
+        return cashe[args]
+    return wrapper
