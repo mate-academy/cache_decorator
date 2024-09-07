97cee9a2c52030afebc0154deeaa2de6124d59cc
Kostia Kryvulia <kostiakryvulia@gmail.com>
2024-09-07 13:23:29 +0300

    Cashe decorator solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    function_cache = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        function_cache_key = (args, tuple(kwargs.items()))
+        if function_cache_key in function_cache:
+            print("Getting from cache")
+            return function_cache[function_cache_key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        function_cache[function_cache_key] = result
+        return result
+    return wrapper
