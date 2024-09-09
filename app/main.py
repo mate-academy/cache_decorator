774c32bab93f7ff2539c3d1c04a18887c28d0ee2
Fedir <fedor.agureev@gmail.com>
2024-09-09 21:42:30 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_list = {}
 
-    def wrapper(*args, **kwargs) -> str:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
         cache_key = (args, tuple(sorted(kwargs.items())), func.__name__,)
 
         if cache_key in cache_list:
