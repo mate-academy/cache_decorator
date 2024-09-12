480e81fdf91650e4118a7ba15ad68ebca58da69d
kostya.harashchuk <garashchukke@gmail.com>
2024-09-12 11:02:21 +0200

    add cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = dict()
+
+    @wraps(func)
+    def cache_wrapper(*args, **kwargs) -> Callable:
+        cache_key = (args, frozenset(kwargs.items()), func.__name__,)
+
+        if cache_key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[cache_key]
+
+        print("Calculating new result")
+        cache_dict[cache_key] = func(*args, **kwargs)
+        return cache_dict[cache_key]
+
+    return cache_wrapper
