ba85b733b001072282eb964deccd9dd8c7c02233
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-09-15 14:11:21 +0300

    Solution py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,13 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result = {}
+
+    def inner(*args) -> int:
+        if args not in result:
+            result[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return func(*args) if args not in result else result[args]
+    return inner
