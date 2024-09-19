709ebeff600f16e6ba1fc1253bb8139af41a57ee
Mike Shmygel <m.shmygel@gmail.com>
2024-09-19 11:33:58 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import functools
 
 def cache(func: Callable) -> Callable:
 
-    decorator_cache = {}
+    cache_storage = {}
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         args += tuple(kwargs.values())
-        if args not in decorator_cache:
+        if args not in cache_storage:
             print("Calculating new result")
-            decorator_cache[args] = func(*args, **kwargs)
+            cache_storage[args] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-        return decorator_cache[args]
+        return cache_storage[args]
     return wrapper
