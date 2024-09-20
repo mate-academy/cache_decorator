42f6ea4f75755c598146f416488e3f671067e666
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-09-20 17:24:50 +0300

    Cache decorator solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,36 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results_cache = []
+
+    def inner(*args) -> Callable:
+        if func(*args) in results_cache:
+            print("Getting from cache ")
+        else:
+            print("Calculating new result ")
+            results_cache.append(func(*args))
+        return func(*args)
+    return inner
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
+
+# Calculating new result
+# Calculating new result
+# Calculating new result
+# Getting from cache
+# Calculating new result
+# Getting from cache
