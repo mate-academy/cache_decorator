81579f5f236620397fefecaaa14b60ed77973798
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-09-21 08:55:30 +0300

    Implemented decorator upd.2

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def cache(func: Callable) -> Callable:
 
         if args in cache_storage:
             print("Getting from cache")
-            result = cache_storage[args]
+            result = cache_storage[args]["result"]
         else:
             print("Calculating new result")
             result = func(*args)
-            cache_storage[args] = result
+            cache_storage[args] = {"args": args, "result": result}
 
         return result
 
