e5fe47d6b9d43637a72cdcb72864cf34f89c42ef
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-09-22 17:06:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrapper(*args) -> Callable:
+        if not all(isinstance(arg, (list, set, dict)) for arg in args):
+            if (func, *args) not in storage:
+                answer = func(*args)
+                storage[(func, *args)] = answer
+                print("Calculating new result")
+                return answer
+            else:
+                print("Getting from cache")
+                return storage[(func, *args)]
+
+    return wrapper
