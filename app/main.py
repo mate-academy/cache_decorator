7aab2407712b77046ef6d7ae89f3e07fd475cca6
Aleksandr <sasha.syrota15@gmail.com>
2024-09-22 15:58:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Any, Callable
+
+data = []
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, **kwargs) -> Any:
+        aray = []
+        aray.append(func)
+        aray.append(args)
+        if aray not in data:
+            data.append(aray)
+            result = func(*args)
+            data.append(result)
+            print("Calculating new result")
+            return result
+        else:
+            print("Getting from cache")
+            return data[data.index(aray) + 1]
+
+    return inner
