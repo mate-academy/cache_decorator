2e931a9711984ccdbee88fa09248b77ebd4b7bdb
Dell <gyrkalomas@gmail.com>
2024-09-23 15:58:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cashed_dict = {}
+
+    def inner(*args, **kwargs) -> dict:
+        key = (args, tuple(kwargs.items()))
+        if key in cashed_dict:
+            print("Getting from cache")
+            return cashed_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cashed_dict[key] = result
+            return result
+    return inner
