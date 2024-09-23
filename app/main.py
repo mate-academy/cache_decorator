1a6b0223d47008571fa44835fa8d70208d1a8d16
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-09-23 14:09:14 +0300

    add kwargs to function

--- app/main.py
+++ app/main.py
@@ -4,18 +4,18 @@ from typing import Callable, Dict, Any
 def cache(func: Callable) -> Callable:
     results_cache: Dict[tuple, Any] = {}
 
-    def inner(*args) -> Callable:
+    def inner(*args, **kwargs) -> Callable:
         try:
             if args in results_cache:
                 print("Getting from cache")
                 return results_cache[args]
             else:
                 print("Calculating new result")
-                result = func(*args)
+                result = func(*args, **kwargs)
                 results_cache[args] = result
                 return result
         except TypeError:
             print("Arguments are not hashable, calculating without caching")
-            return func(*args)
+            return func(*args, **kwargs)
 
     return inner
