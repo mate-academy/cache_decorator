149481ff219482ee615e1d428fcc1d9022aa2527
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-09-23 14:18:07 +0300

    fixed solution using kwargs

--- app/main.py
+++ app/main.py
@@ -5,14 +5,20 @@ def cache(func: Callable) -> Callable:
     results_cache: Dict[tuple, Any] = {}
 
     def inner(*args, **kwargs) -> Callable:
+        if kwargs:
+            kwargs_tuple = tuple(sorted(kwargs.items()))
+        else:
+            kwargs_tuple = ()
+
+        cache_key = args + kwargs_tuple
         try:
             if args in results_cache:
                 print("Getting from cache")
-                return results_cache[args]
+                return results_cache[cache_key]
             else:
                 print("Calculating new result")
                 result = func(*args, **kwargs)
-                results_cache[args] = result
+                results_cache[cache_key] = result
                 return result
         except TypeError:
             print("Arguments are not hashable, calculating without caching")
