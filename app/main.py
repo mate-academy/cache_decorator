f030d7edeebcc43ee8c446699880dbb15e8d5b30
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-09-24 19:57:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,14 +5,11 @@ def cache(func: Callable) -> Callable:
     storage = {}
 
     def wrapper(*args) -> Callable:
-        if not all(isinstance(arg, (list, set, dict)) for arg in args):
-            if (func, *args) not in storage:
-                answer = func(*args)
-                storage[(func, *args)] = answer
-                print("Calculating new result")
-                return answer
-            else:
-                print("Getting from cache")
-                return storage[(func, *args)]
-
+        if (func, *args) not in storage:
+            answer = func(*args)
+            storage[(func, *args)] = answer
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return storage[(func, *args)]
     return wrapper
