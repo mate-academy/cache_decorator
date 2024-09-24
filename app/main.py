1c803fe6c568c3af1255bfef9adf21992c4233f9
Yurii <yurazosia@gmail.com>
2024-09-24 22:55:39 +0300

    Add one return to wrapper

--- app/main.py
+++ app/main.py
@@ -2,21 +2,17 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Словник для зберігання результатів, - наш Кеш,
-    # де ключі це аргументи, а значення результат функції.
     cache_dict = {}
 
     def wrapper(*args: Any) -> Any:
-        # Якщо аргументи вже є в кеші, повертаємо збережений результат
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
+            result = cache_dict[args]
         else:
-            # Якщо аргументів немає в кеші, обчислюємо новий результат
-            # та зберігаємо його в наш кеш.
             print("Calculating new result")
             result = func(*args)
             cache_dict[args] = result
-            return result
+
+        return result
 
     return wrapper
