6eae5633fc82ad75e4167c136175eb652145e32a
Yurii <yurazosia@gmail.com>
2024-09-24 22:37:25 +0300

    Add comments

--- app/main.py
+++ app/main.py
@@ -2,13 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
+    # Словник для зберігання результатів, - наш Кеш,
+    # де ключі це аргументи, а значення результат функції
     cache_dict = {}
 
     def wrapper(*args):
+        # Якщо аргументи вже є в кеші, повертаємо збережений результат
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
         else:
+            # Якщо аргументів немає в кеші, обчислюємо новий результат
+            # та зберігаємо його в наш кеш
             print("Calculating new result")
             result = func(*args)
             cache_dict[args] = result
