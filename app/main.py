07be5a375ac572ced411037de74de2f60cdb9e03
Max <mxbely98@gmail.com>
2024-09-25 15:31:35 +0300

    change approach to resolve

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Any:
         if args in uniques_names_functions.keys():
             print("Getting from cache")
-            return uniques_names_functions[*args]
+            return uniques_names_functions[args]
         else:
-            uniques_names_functions[*args] = func(*args)
+            uniques_names_functions[args] = func(*args)
             print("Calculating new result")
-            return uniques_names_functions[*args]
+            return uniques_names_functions[args]
     return inner
