daaf646b7c0c98fc2bcb104c0e749aa57b44f902
Yurii <yurazosia@gmail.com>
2024-09-25 15:05:17 +0300

    Add wraps and kwargs

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args: Any) -> Any:
-        if args in cache_dict:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        cache_dict_key = (args, tuple(sorted(kwargs.items())))
+        if cache_dict_key in cache_dict:
             print("Getting from cache")
-            result = cache_dict[args]
+            result = cache_dict[cache_dict_key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_dict[args] = result
+            result = func(*args, **kwargs)
+            cache_dict[cache_dict_key] = result
 
         return result
 
