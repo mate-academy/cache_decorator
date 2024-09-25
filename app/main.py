3fb9f93b323abaca2e219a8f1e0dfcc5e08d9ff6
Dima Havrylenko <94nj111@gmail.com>
2024-09-25 14:45:08 +0300

    implement cache-decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_memory = {}
+
+    def inner(*args) -> int:
+        key = tuple([*args])
+        if key in cache_memory:
+            print("Getting from cache")
+            return cache_memory[key]
+        cache_memory[key] = func(*args)
+        print("Calculating new result")
+        return cache_memory[key]
+    return inner
