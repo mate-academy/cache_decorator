98051a2d5b1e440eca60b3ac160642062261ec38
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2024-09-25 20:33:48 +0300

    code_changed

--- app/main.py
+++ app/main.py
@@ -11,19 +11,17 @@ def cache(func: Callable) -> Callable:
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        return result
     return wrapper
 
 
-@cache
 def long_time_func(base: int, exponent: int, modules: int) -> int:
     return (base ** exponent ** modules) % (base * modules)
 
 
-@cache
 def long_time_func_2(n_tuple: tuple, power: int) -> list:
     return [number ** power for number in n_tuple]
