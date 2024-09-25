917c9f1f9422fe5cc98a1114a5505279d0be27dc
BSDL <blackharbor@dontsp.am>
2024-09-25 13:43:48 +0300

    Added use of **kwargs && Removed extra code fragments

--- app/main.py
+++ app/main.py
@@ -6,32 +6,16 @@ def cache(func: Callable) -> Callable:
     cache_data = {}
 
     @wraps(func)
-    def wrapper(*args) -> None:
-        if args in cache_data:
+    def wrapper(*args, **kwargs) -> None:
+        cache_key = (args, frozenset(kwargs.items()))
+
+        if cache_key in cache_data:
             print("Getting from cache")
-            return cache_data[args]
+            return cache_data[cache_key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_data[args] = result
+            result = func(*args, **kwargs)
+            cache_data[cache_key] = result
             return result
 
     return wrapper
-
-
-@cache
-def long_time_func(num_a: int, num_b: int, num_c: int) -> int:
-    return (num_a ** num_b ** num_c) % (num_a * num_c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
-
-
-print(long_time_func(1, 2, 3))
-print(long_time_func(2, 2, 3))
-print(long_time_func_2((5, 6, 7), 5))
-print(long_time_func(1, 2, 3))
-print(long_time_func_2((5, 6, 7), 10))
-print(long_time_func_2((5, 6, 7), 10))
