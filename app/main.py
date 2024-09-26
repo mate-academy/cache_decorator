eacaa199289da3fd20345099ed5a418395d9aa05
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-09-26 11:44:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_dict = {}
+
+    @wraps(func)
+    def wrapper_cash(*args, **kwargs) -> Any:
+        key = args + tuple(kwargs.items())
+        if key in cash_dict:
+            print("Getting from cache")
+            return cash_dict[key]
+
+        print("Calculating new result")
+        cash_value = func(*args, **kwargs)
+        cash_dict[key] = cash_value
+
+        return cash_value
+
+    return wrapper_cash
