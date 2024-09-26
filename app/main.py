6263d56507884f1d751e8a61f5aeb74f3c6f4929
Max <mxbely98@gmail.com>
2024-09-26 13:46:21 +0300

    simplification resolution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,13 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    uniques_names_functions = {}
+    storage = {}
 
     def inner(*args) -> Any:
-        if args in uniques_names_functions.keys():
+        if args in storage.keys():
             print("Getting from cache")
-            return uniques_names_functions[args]
         else:
-            uniques_names_functions[args] = func(*args)
+            storage[args] = func(*args)
             print("Calculating new result")
-            return uniques_names_functions[args]
+        return storage[args]
     return inner
