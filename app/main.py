a1e2535f6fa092874a88376f28330f975da64023
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2024-09-26 14:45:59 +0300

    feat: implemented cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def wrapper(*args, **kwargs) -> Any:
+        cache_key = args + tuple(kwargs.items())
+        if cache_key not in wrapper.cache:
+            print("Calculating new result")
+            wrapper.cache[cache_key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return wrapper.cache[cache_key]
+    wrapper.cache = {}
+    return wrapper
