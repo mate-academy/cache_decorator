0483e3dc220c23249b04d8b0c0c9925fcd6e11fe
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-09-27 14:22:45 +0300

    Decorator solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        if args not in cache_storage:
+            print("Calculating new result")
+            function_result = func(*args, **kwargs)
+            cache_storage[args] = function_result
+            return function_result
+        else:
+            print("Getting from cache")
+            return cache_storage.get(args)
+
+    wrapper.cache_storage = cache_storage
+    return wrapper
