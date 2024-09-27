63c26a32b8a9d62923a047b9c8af309bb40314c3
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-09-27 14:41:29 +0300

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -7,12 +7,9 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args, **kwargs) -> Any:
         if args not in cache_storage:
             print("Calculating new result")
-            function_result = func(*args, **kwargs)
-            cache_storage[args] = function_result
-            return function_result
+            cache_storage[args] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-            return cache_storage.get(args)
 
-    wrapper.cache_storage = cache_storage
+        return cache_storage.get(args)
     return wrapper
