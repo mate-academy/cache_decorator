6b8e2549c72dedb2f84abe9409b796c4f17336a6
Bondzik-S <bondziks@gmail.com>
2024-09-27 17:35:38 +0300

    Cashe-Decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from cgitb import reset
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args) -> Any:
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+
+        print("Calculating new result")
+        result = func(*args)
+
+        cache_dict[args] = result
+
+        return result
+
+    return wrapper
+
