0b3dc7c987c7c3730969988b5b0a00df66e5e395
Den_k0 <denys.kotovskiy@gmail.com>
2024-09-27 22:19:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_data = {}
 
-    def wrapper(*args, **kwargs) -> Any:
-        data = (args, tuple(sorted(kwargs.items())))
-
-        if data not in cache_data:
+    def wrapper(*args) -> Any:
+        if args not in cache_data:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_data[data] = result
+            result = func(*args)
+            cache_data[args] = result
         else:
             print("Getting from cache")
 
-        return cache_data[data]
+        return cache_data[args]
 
     return wrapper
