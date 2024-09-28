10e9e00be923f7b019c6adba45600962b8bc08ca
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-09-28 22:15:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    collected_data = {}
+
+    def wrapper(*args, **kwargs) -> int:
+
+        if args not in collected_data:
+            function_result = func(*args, **kwargs)
+            collected_data[args] = function_result
+            print("Calculating new result")
+            return function_result
+
+        print("Getting from cache")
+        return collected_data[args]
+
+    return wrapper
