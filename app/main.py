48405ef8c3293f81145d88884ab85d329e40ad15
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-09-29 21:55:21 +0300

    Remove **kwargs

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     collected_data = {}
 
-    def wrapper(*args, **kwargs) -> int:
+    def wrapper(*args) -> int:
 
         if args not in collected_data:
-            function_result = func(*args, **kwargs)
+            function_result = func(*args)
             collected_data[args] = function_result
             print("Calculating new result")
             return function_result
