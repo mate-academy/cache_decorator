c789bab5384547b60937752551d0b47b2003bd95
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-09-30 13:07:52 +0300

    Refactor

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     collected_data = {}
 
-    def wrapper(*args) -> int:
+    def wrapper(*args) -> Any:
 
         if args not in collected_data:
             function_result = func(*args)
             collected_data[args] = function_result
             print("Calculating new result")
-            return function_result
+        else:
+            print("Getting from cache")
 
-        print("Getting from cache")
         return collected_data[args]
 
     return wrapper
