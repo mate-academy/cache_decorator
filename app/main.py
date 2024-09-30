6c3539b2bb7da3ca6768b102d69d1371efb98837
Bondzik-S <bondziks@gmail.com>
2024-09-30 13:58:49 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -7,13 +7,11 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> Any:
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
-
-        print("Calculating new result")
-        result = func(*args)
-
-        cache_dict[args] = result
+            result = cache_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_dict[args] = result
 
         return result
-
     return wrapper
