029c3850b03b4649f317ca7e24ee858133afa00c
Oleksandr Kozlov <aleksandrkozlov2015@gmail.com>
2024-10-04 22:11:04 +0200

    'Solution-changes_1'

--- app/main.py
+++ app/main.py
@@ -1,42 +1,20 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     res_dict = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
-        if args in res_dict:
+        key = (args, tuple(kwargs.items()), func.__name__,)
+        if key in res_dict:
             print("Getting from cache")
-            return res_dict[args]
+            return res_dict[key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            res_dict[args] = result
+            result = func(*args, **kwargs)
+            res_dict[key] = result
             return result
 
     return wrapper
-
-
-@cache
-def long_time_func(a_a: int, b_b: int, c_c: int) -> int:
-    return (a_a ** b_b ** c_c) % (a_a * c_c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
-
-# Calculating new result
-# Calculating new result
-# Calculating new result
-# Getting from cache
-# Calculating new result
-# Getting from cache
