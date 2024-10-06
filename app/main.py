434675daf39b830dea5c7060b9febd4840443e7e
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-10-06 22:19:05 +0300

    removed unnecessary check

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ def cache(func: Callable) -> Callable:
 
     def inner(*args, **kwargs) -> Callable:
         key_in_save_result = (func.__name__, args, tuple(kwargs.items()))
-        if save_result:
-            if key_in_save_result in save_result:
-                print("Getting from cache")
-                return save_result[key_in_save_result]
+
+        if key_in_save_result in save_result:
+            print("Getting from cache")
+            return save_result[key_in_save_result]
 
         print("Calculating new result")
         result = func(*args, **kwargs)
