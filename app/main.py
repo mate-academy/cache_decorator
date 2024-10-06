e4935e491ade6b0c77c662559b01d887345f4078
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-10-06 21:50:14 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -5,16 +5,14 @@ def cache(func: Callable) -> Callable:
     save_result = {}
 
     def inner(*args, **kwargs) -> Callable:
-        key_in_save_result = (func.__name__, args,)
-        for value in kwargs.values():
-            key_in_save_result.add(value)
+        key_in_save_result = (func.__name__, args, tuple(kwargs.items()))
         if save_result:
             if key_in_save_result in save_result:
                 print("Getting from cache")
                 return save_result[key_in_save_result]
 
         print("Calculating new result")
-        result = func(*args)
+        result = func(*args, **kwargs)
         save_result[key_in_save_result] = result
         return result
 
