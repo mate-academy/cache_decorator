372fe39888cd5edcaa200c4ad1506505637624c9
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-10-06 20:31:11 +0300

    DRY and add kwargs

--- app/main.py
+++ app/main.py
@@ -4,21 +4,18 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     save_result = {}
 
-    def inner(*args) -> Callable:
+    def inner(*args, **kwargs) -> Callable:
+        key_in_save_result = (func.__name__, args,)
+        for value in kwargs.values():
+            key_in_save_result.add(value)
         if save_result:
-            key_in_save_result = (func, args,)
             if key_in_save_result in save_result:
                 print("Getting from cache")
                 return save_result[key_in_save_result]
-            else:
-                print("Calculating new result")
-                result = func(*args)
-                save_result[(func, args,)] = result
-                return result
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            save_result[(func, args,)] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args)
+        save_result[key_in_save_result] = result
+        return result
 
     return inner
