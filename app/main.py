bde88d0777cdfe69a1dc7998368ae0f6e6f33cf3
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-10-07 17:49:53 +0300

    Update app/main.py
    Co-authored-by: Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -11,9 +11,8 @@ def cache(func: Callable) -> Callable:
 
         if key in cache_dict:
             print("Getting from cache")
-            return cache_dict[key]
-
-        print("Calculating new result")
-        cache_dict[key] = func(*args, **kwargs)
+        else:
+            print("Calculating new result")
+            cache_dict[key] = func(*args, **kwargs)
         return cache_dict[key]
     return wrapper
