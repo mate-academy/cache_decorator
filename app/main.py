7d66ae51339038483e1c372e1af9b1a9c9d976de
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2024-10-07 21:57:16 +0300

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
-    def wrapper(*args) -> Any:
-        if args in cache_storage:
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in cache_storage:
             print("Getting from cache")
-            return cache_storage[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            cache_storage[args] = result
-            return result
+            return cache_storage[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_storage[key] = result
+        return result
     return wrapper
