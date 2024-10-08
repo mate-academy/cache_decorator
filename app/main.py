324f4c1d4d1a09e382c87175f454a1db70d92829
Anton Savka <anton.savka.business@gmail.com>
2024-10-09 01:30:15 +0300

    Provided the solution

--- app/main.py
+++ app/main.py
@@ -6,12 +6,13 @@ def cache(func: Callable) -> Callable:
     dict_cache = {}
 
     @functools.wraps(func)
-    def wrapper(*args: Any) -> Any:
-        if args not in dict_cache:
-            dict_cache[args] = func(*args)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (func.__name__, args, tuple(kwargs.items()))
+        if key not in dict_cache:
+            dict_cache[key] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return dict_cache[args]
+        return dict_cache[key]
 
     return wrapper
