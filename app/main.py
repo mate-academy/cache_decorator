f03661517aae01ecbe544c6cbf1c41767b50e2af
srx <srx92@protonmail.com>
2024-10-10 15:22:41 +0300

    Update solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
-    def inner(*args) -> str:
+    @functools.wraps(func)
+    def inner(*args, **kwargs) -> Any:
         if func.__name__ not in cached_results:
-            print("Calculating new result")
-            cached_results[func.__name__] = {args: func(*args)}
+            cached_results[func.__name__] = {}
+        if args in cached_results[func.__name__]:
+            print("Getting from cache")
         else:
-            if args in cached_results[func.__name__]:
-                print("Getting from cache")
-            else:
-                print("Calculating new result")
-                cached_results[func.__name__][args] = func(*args)
+            print("Calculating new result")
+            cached_results[func.__name__][args] = func(*args)
 
         return cached_results[func.__name__][args]
 
