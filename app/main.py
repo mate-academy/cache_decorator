ad704e1da55b81eb6a990df34cc05296db0b8477
Piotr Zegarek <pzegarek@intel471.com>
2024-10-19 18:37:35 +0200

    solution'
    '

--- app/main.py
+++ app/main.py
@@ -2,5 +2,19 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    """Caching decorator."""
+    cache_store = {}  # Dictionary to store cached results
+
+    def wrapper(*args) -> Callable:
+        # Check if the result for given arguments is already cached
+        if (func, args) in cache_store:
+            print("Getting from cache")
+            return cache_store[(func, args)]
+        else:
+            # If not cached, calculate the result and store it in the cache
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[(func, args)] = result
+            return result
+
+    return wrapper
