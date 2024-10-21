cb11a82e6cc9c0350ca7e7fa567cb7aacaa529cc
Greena1c <shchedrykserhii@gmail.com>
2024-10-22 01:55:30 +0300

    Completing the task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Dict, Any, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_storage: Dict[Tuple[Any, ...], Any] = {}
+
+    def wrapper(*args) -> Any:
+        if args in cash_storage:
+            print("Getting from cache")
+            return cash_storage[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cash_storage[args] = result
+            return result
+    return wrapper
