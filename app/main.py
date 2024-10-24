49181342c888143e8993c49c8ced5d9651bc4dc7
Greena1c <shchedrykserhii@gmail.com>
2024-10-24 17:02:20 +0300

    Error correction

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ from typing import Callable, Dict, Any, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    cash_storage: Dict[Tuple[Any, ...], Any] = {}
+    cache_storage: Dict[Tuple[Any, ...], Any] = {}
 
-    def wrapper(*args) -> Any:
-        if args in cash_storage:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in cache_storage:
             print("Getting from cache")
-            return cash_storage[args]
+            return cache_storage[key]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cash_storage[args] = result
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
             return result
+
     return wrapper
