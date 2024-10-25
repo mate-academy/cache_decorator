17198e8de17fad8f6100e2cf79a94350beb5c5da
Iryna Mishchenko <djrobentab@gmail.com>
2024-10-25 03:15:17 +0200

    Add Any

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
