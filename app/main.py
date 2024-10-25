2fb583fc44a93da4d808b95ee3c2b1f09ba06afe
Iryna Mishchenko <djrobentab@gmail.com>
2024-10-25 02:02:17 +0200

    Add **kwargs

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args) -> Callable:
+    def wrapper(*args, **kwargs) -> Callable:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cache_dict[args] = result
             return result
     return wrapper
