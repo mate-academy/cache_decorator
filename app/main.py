48c5f042c3791d6f298a5b3c1ab47fbffa3918c0
bc2024-4 <bzabrodskij20@gmail.com>
2024-10-28 19:25:01 +0200

    add program

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper(*args) -> Any:
+        # Ініціалізуємо кеш, якщо він не існує як атрибут wrapper
+        if not hasattr(wrapper, "_cache"):
+            wrapper._cache = {}
+
+        # Перевіряємо, чи є результат для цих аргументів у кеші
+        if args in wrapper._cache:
+            print("Getting from cache")
+            return wrapper._cache[args]
+        else:
+            print("Calculating new result")
+            # Обчислюємо та зберігаємо результат, якщо його немає в кеші
+            result = func(*args)
+            wrapper._cache[args] = result
+            return result
+    return wrapper
