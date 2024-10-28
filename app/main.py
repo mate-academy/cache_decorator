28232812b46af0604f62c06193386dccf4bac620
Mariana <mariana.antoniak.babish@gmail.com>
2024-10-28 08:30:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from typing import Callable
 
 
-def cache(*func: Callable) -> Callable:
+def cache(func: Callable) -> Callable:
     result = {}
-    for fun in func:
-        def decorate(*args) -> None:
-            if len(result) == 0 or args not in result:
-                print("Calculating new result")
-                new_value = fun(*args)
-                result[args] = new_value
-                return new_value
-            else:
-                print("Getting from cache")
-                return result[args]
-            return result
-        return decorate
+
+    def decorate(*args) -> None:
+        if len(result) == 0 or args not in result:
+            print("Calculating new result")
+            new_value = func(*args)
+            result[args] = new_value
+            return new_value
+        else:
+            print("Getting from cache")
+            return result[args]
+    return decorate
+    return result
