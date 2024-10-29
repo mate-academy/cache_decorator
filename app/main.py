1c17d1ce440f0dd72b3c890000e1f178595b3fb5
andriiplaksin <nexizzq@gmail.com>
2024-10-29 17:04:08 +0100

    solving the task.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-from typing import Callable
+import functools
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_results = {}
+
+    @functools.wraps(func)
+    def wrapper(*args: tuple) -> Any:
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
+
+        print("Calculating new result")
+        result = func(*args)
+        cached_results[args] = result
+        return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
