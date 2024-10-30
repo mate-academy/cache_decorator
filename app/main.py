b5b5f94c879b5a8fef8cd38b1eba7ba5a9afa153
Danylo Myroshnychenko <danil010701@icloud.com>
2024-10-30 04:11:11 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from typing import Callable
 
-
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrapper(*args):
+        if args in storage:
+            print("Getting from cache")
+            return storage[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            storage[args] = result
+            return result
+
+    return wrapper
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+long_time_func(1, 2, 3)
+long_time_func(1, 2, 3)
