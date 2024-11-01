809d87f9187d0e0fcaeb650e76c4ebdb2f186a94
Beliar24 <blackdid7@gmail.com>
2024-11-02 01:01:14 +0200

    cache function implemented

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Callable:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
