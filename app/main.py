c8da4375510fe40d68f43c4eafb7e3e0c04a38da
Julia Alexeeva <julia4406@gmail.com>
2024-11-05 18:11:37 +0200

    add text output ('Calculating new result' and 'Getting from cache').

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def cache(func: Callable) -> Callable:
         if parameters not in cached_data.keys():
             result = func(*args, **kwargs)
             cached_data[parameters] = result
+            print("Calculating new result")
             return result
         else:
+            print("Getting from cache")
             return cached_data.get(parameters)
     return wrapper
