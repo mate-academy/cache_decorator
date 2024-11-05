480dc9824a30de23f1a1ce6ec56ae2d8735fe645
Julia Alexeeva <julia4406@gmail.com>
2024-11-05 18:04:18 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_data = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        parameters = tuple(args)
+        if parameters not in cached_data.keys():
+            result = func(*args, **kwargs)
+            cached_data[parameters] = result
+            return result
+        else:
+            return cached_data.get(parameters)
+    return wrapper
