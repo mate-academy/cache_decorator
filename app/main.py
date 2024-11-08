d12d12127796e71ce98152370771581e06a57fa9
Viacheslav Seredenko <vrungelsp@gmail.com>
2024-11-08 14:06:36 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result = {}
+
+    @wraps(func)
+    def inner(*args) -> int:
+        if (func.__name__, args) not in result:
+            result[(func.__name__, args)] = \
+                result.get((func.__name__, args), func(*args))
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return result[(func.__name__, args)]
+
+    return inner
