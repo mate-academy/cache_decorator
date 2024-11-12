40acb648ea9ad0aaddb93ae7c16f1541f8d911ab
ComfortCityBud <gleb@gmail.com>
2024-11-12 21:06:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,27 +5,12 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        flag = True
-        while flag:
-            data_iterable = tuple(
-                [item for item in args if isinstance(item, (int, str, float))]
-            )
-            if data_iterable not in cache_dict:
-                print("Calculating new result")
-                cache_dict[data_iterable] = func(*args, **kwargs)
-                return cache_dict[data_iterable]
-            else:
-                print("Getting from cache")
-                return cache_dict[data_iterable]
+        data_iterable = (args, frozenset(kwargs.items()))
+        if data_iterable not in cache_dict:
+            print("Calculating new result")
+            cache_dict[data_iterable] = func(*args, **kwargs)
+            return cache_dict[data_iterable]
+        print("Getting from cache")
+        return cache_dict[data_iterable]
 
     return wrapper
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:  # NOQA VNE001
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
