afda5661a67587b0b65842c3da3be3f477ceb518
Oksana <oksana.zhornyk@gmail.com>
2024-11-12 18:42:18 +0100

    return type for wrapper has been changed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_args = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(kwargs.items()))
         if key in cache_args:
             print("Getting from cache")
