2dc274d0f0176efba1e88bf3208e9e8b46c672e3
Vova <vova.gld05@gmail.com>
2024-11-12 18:06:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrapper(*args: list) -> Any:
+        if args in storage:
+            print("Getting from cache")
+            return storage[args]
+        else:
+            result = func(*args)
+            storage[args] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
