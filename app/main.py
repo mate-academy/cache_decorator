b5a164197c3b0eb5de717e3e9c83ce549edefabe
fredisson11 <fredisson11@protonmail.com>
2024-11-14 15:35:15 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    data_cash = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+
+        nonlocal data_cash
+
+        if key in data_cash:
+            print("Getting from cache")
+            return data_cash[key]
+        else:
+            func_result = func(*args, **kwargs)
+            data_cash[key] = func_result
+            print("Calculating new result")
+
+            return func_result
+
+    return wrapper
