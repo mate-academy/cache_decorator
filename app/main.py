4246ce3b3dd6a1eef743eee236f7fc03185ec622
neeeksa <avgust.k31@gmail.com>
2024-11-16 20:18:51 +0200

    Solution with immutable types are used as keys

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from typing import Callable
 
 
+def make_hashable(obj: (list, dict, set)) -> (tuple, frozenset):
+    """Преобразует объект в хэшируемую структуру."""
+    if isinstance(obj, list):
+        return tuple(obj)
+    elif isinstance(obj, dict):
+        return tuple(sorted(obj.items()))
+    elif isinstance(obj, set):
+        return frozenset(obj)
+    else:
+        return obj
+
+
 def cache(func: Callable) -> Callable:
     storage = {}
 
     def wrapper(*args) -> Callable:
-        try:
-            key = tuple(args)
-        except TypeError as e:
-            raise ValueError(f"All arguments must be hashable: {e}")
+        key = tuple(make_hashable(arg) for arg in args)
 
         if key in storage:
             print("Getting from cache")
