c83c0e59e7bba06450fd89a8454e17bf1aacfea6
Serhii <annolel@gmail.com>
2024-11-18 16:21:40 +0100

    solution to py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    tmp_cache = {}
+
+    def inner(*args) -> None:
+        if args in tmp_cache:
+            print("Getting from cache")
+            return tmp_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            tmp_cache[args] = result
+            return result
+    return inner
