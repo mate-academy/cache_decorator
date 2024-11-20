8a9f471b34d4ac25f7da1a31f5adf98d939efb67
Dolgov Dmytro <dimakherson74@gmail.com>
2024-11-20 16:38:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner():
+        func()
+    return inner
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
