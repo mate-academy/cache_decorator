faada874e3a39b8a32b12daa333db8cdea138f8f
Dolgov Dmytro <dimakherson74@gmail.com>
2024-11-21 15:18:33 +0200

    Solution_hashable_args

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 from typing import Callable
 
 
+def make_hashable(args):
+    hashable_args = []
+    for arg in args:
+        if isinstance(arg, (list, set)):
+            hashable_args.append(tuple(arg))
+        elif isinstance(arg, dict):
+            hashable_args.append(tuple(sorted(arg.items())))
+        else:
+            hashable_args.append(arg)
+    return tuple(hashable_args)
+
+
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
     def inner(*args) -> Callable:
-        if args in cache_store:
+        hashable_args = make_hashable(args)
+        if hashable_args in cache_store:
             print("Getting from cache")
-            return cache_store[args]
+            return cache_store[hashable_args]
         print("Calculating new result")
         result = func(*args)
-        cache_store[args] = result
+        cache_store[hashable_args] = result
         return result
     return inner
