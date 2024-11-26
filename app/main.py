056a01b5f5d20679c0748a3d1fc1a4eb89a1049c
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-11-26 12:02:33 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -4,11 +4,10 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_saved_results = {}
 
-    def wrapper(*args) -> int:
+    def wrapper(*args) -> any:
         if args in cache_saved_results:
             print("Getting from cache")
             return cache_saved_results[args]
         print("Calculating new result")
-        cache_saved_results[args] = func(*args)
-        return cache_saved_results[args]
+        return cache_saved_results.setdefault(args, func(*args))
     return wrapper
