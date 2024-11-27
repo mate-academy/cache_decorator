769e61ceb178e10d81cf6e24d39dcfb42cbcf15d
Diana Zatvarska <dizatvarska@gmail.com>
2024-11-27 12:45:40 -0800

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ def cache(func: Callable) -> Any:
     results = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        key = tuple(args, *kwargs.items())
+        key = tuple(args)
+        key += tuple(*kwargs.items())
         if key in results:
             print("Getting from cache")
             return results[key]
-        else:
-            result = func(*args, **kwargs)
-            results[key] = result
-            print("Calculating new result")
-            return result
+        result = func(*args, **kwargs)
+        results[key] = result
+        print("Calculating new result")
+        return result
     return wrapper
