db4aa66e5f87cf8200c930372144d04d8956468a
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-11-27 18:17:11 +0200

    'Remove_return'

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> any:
         if args in cache_saved_results:
             print("Getting from cache")
-            return cache_saved_results[args]
-        print("Calculating new result")
-        return cache_saved_results.setdefault(args, func(*args))
+        else:
+            print("Calculating new result")
+            cache_saved_results[args] = func(*args)
+        return cache_saved_results[args]
     return wrapper
