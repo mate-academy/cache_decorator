937375fef0b639bc30ffa4e016fe51361e7bae1b
taraskhom <khomenkotaras1305@gmail.com>
2024-11-28 16:44:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,21 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    cash_data_check = {}
+
+    def wrapper(*args) -> None:
+
+        nonlocal cash_data_check
+        cashed_data = args
+
+        if cashed_data in cash_data_check:
+            print("Getting from cache")
+            return cash_data_check.get(cashed_data)
+        else:
+            result = func(*args)
+            cash_data_check.update({cashed_data: result})
+            print("Calculating new result")
+            return result
+
+    return wrapper
