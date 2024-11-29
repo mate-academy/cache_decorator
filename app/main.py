64f98330a8359b2702a7ec159c03d33051419e7a
git_practic <dogoberrr@gmail.com>
2024-11-29 18:25:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def wrapper(*args) -> Callable:
+        if args in cache_data.keys():
+            print("Getting from cache")
+            return cache_data[args]
+
+        cache_data[args] = func(*args)
+        print("Calculating new result")
+        return cache_data[args]
+
+    return wrapper
