94aea76669e4f7cc608f4ba8a0e40df1729dff65
4ikkamony <compbanderloga@gmail.com>
2024-11-29 14:13:52 +0200

    Removed left-over comments

--- app/main.py
+++ app/main.py
@@ -3,12 +3,10 @@ from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # {func: {args: result}}
     stored_results = {}
 
     @wraps(func)
     def wrapper(*args) -> dict:
-        # print(stored_results)
         if args not in stored_results:
             print("Calculating new result")
             stored_results[args] = func(*args)
