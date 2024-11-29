159dcc3bad41d6f417e6f6200fdc11271794e818
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-11-29 23:44:32 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cashed_data = {}
+    cached_data = {}
 
     def wrapper(*args) -> None:
 
         cache_data_check = args
 
-        if cache_data_check in cashed_data:
+        if cache_data_check in cached_data:
             print("Getting from cache")
         else:
             result = func(*args)
-            cashed_data.update({cache_data_check: result})
+            cached_data.update({cache_data_check: result})
             print("Calculating new result")
-        return cashed_data[cache_data_check]
+        return cached_data[cache_data_check]
     return wrapper
