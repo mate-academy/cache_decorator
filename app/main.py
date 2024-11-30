9957f357bcd25dd3dd04f6e81d402e2249dbeccc
Natalia <ngayka@gmail.com>
2024-11-30 16:02:56 +0200

    Revert "Solution is ready"

--- app/main.py
+++ app/main.py
@@ -1,19 +1,6 @@
 from typing import Callable
-from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    cache_storage = {}
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        cache_key = (args, tuple(sorted(kwargs.items())))
-        if cache_key in cache_storage:
-            print("Getting from cache")
-            return cache_storage[cache_key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_storage[cache_key] = result
-            return result
-    return wrapper
+    # Write your code here
+    pass
