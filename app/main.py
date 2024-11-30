e57c7e0cede1fa6564dfad51e24643cd52b74238
Yurii Lesyk <lesik.yurko@gmail.com>
2024-11-30 16:30:09 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Dict, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store: Dict[Tuple, any] = {}
+
+
+    def wrapper(*args):
+        if args in cache_store:
+            print("Getting from cache")
+            return cache_store[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[args] = result
+            return result
+    return wrapper()
