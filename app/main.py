f80b1cd0931269c2f75007d5343306f44d31bc49
Admin <mr.artem450@gmail.com>
2024-12-01 14:35:34 +0200

    Task completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        key = (args, tuple(kwargs.items()))
+
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            return result
+
+    return wrapper
