5e35fd22429dbd6b82dc7cff3065b00a88b2cde7
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2024-12-02 20:14:48 +0000

    created decorator for task

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_dict = {}
+
+    def inner(*args, **kwargs) -> int:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in result_dict:
+            print("Getting from cache")
+            return result_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            result_dict[key] = result
+            return result
+    return inner
