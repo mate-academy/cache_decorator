f7828d60e41b5bc608b99b2c166820cfeae1da42
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-03 21:25:54 +0200

    Try to fix Valentine's remarks ver.2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def wrapper(*args) -> Callable:
-
+    def wrapper(*args) -> Any:
         if args in cached_data:
             print("Getting from cache")
-            return cached_data[args]
+
         else:
             print("Calculating new result")
             cached_data[args] = func(*args)
-            return cached_data[args]
+        return cached_data[args]
 
     return wrapper
