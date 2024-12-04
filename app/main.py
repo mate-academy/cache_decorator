2bdee44b9fa85631890adb30a99a368882df5124
10kkyvl <ztkkre@gmail.com>
2024-12-04 10:07:11 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ def cache(func: Callable) -> Callable:
         key = (args, tuple(kwargs.items()))
         if key in cached:
             print("Getting from cache")
-            return cached[key]
-
-        print("Calculating new result")
-        result = func(*args, **kwargs)
-        cached[key] = result
+            result = cached[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached[key] = result
         return result
 
     return wrapper
