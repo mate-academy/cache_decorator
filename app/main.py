a1c438c4a1cbe4a9f9ad7cf64ea60e7285cd6fc2
Your NameMykola <mykola.osolinskyi@gmail.com>
2024-12-04 18:34:07 +0200

    fixed according to the mentor comments

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ def cache(func: Callable) -> Callable:
     history = {}
 
     def wrapper(*args) -> Callable:
-        if isinstance(args, (int, float, str, tuple)):
-            if args in history:
-                print("Getting from cache")
-                return history[args]
-            else:
-                result = func(*args)
-                print("Calculating new result")
-                history[args] = result
-                return result
+        if args in history:
+            print("Getting from cache")
+            result = history[args]
+        else:
+            result = func(*args)
+            print("Calculating new result")
+            history[args] = result
+
+        return result
 
     return wrapper
