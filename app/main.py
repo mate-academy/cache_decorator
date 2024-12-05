3612499661e8105905a00d44a710279e900f931a
Dmytro <dimkanividimka4@gmail.com>
2024-12-05 13:42:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
 from typing import Callable
+# import time
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    our_dict = {}
+
+    def wrapper(*args) -> int:
+
+        result = func(*args)
+        if args not in our_dict:
+            print("Calculating new result")
+            our_dict[args] = result
+        else:
+            print("Getting from cache")
+            return our_dict[args]
+
+        return result
+
+    return wrapper
+
+
+# @cache
+# def delay_addition(a, b):
+#     time.sleep(3)
+#     return a + b
+#
+# time1 = time.time()
+# delay_addition(1, 2)
+# time2 = time.time()
+# delay_addition(1, 2)
+# time3 = time.time()
+# print(time1)
+# print(time2)
+# print(time3)
