c4e95cf5abd912fa9e11e44933cfe437ba15a249
Volodymir Shlikhta <shlikhta77@gmail.com>
2024-12-05 20:01:51 +0200

    my decorator cash

--- app/main.py
+++ app/main.py
@@ -2,5 +2,19 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+
+        if key in cache_data:
+            print("Getting from cache")
+            return cache_data[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_data[key] = result
+        return result
+
+    return wrapper
+
