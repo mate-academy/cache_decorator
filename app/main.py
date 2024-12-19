c8a83ab2f329972b380329b0102014522fa37dfd
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2024-12-19 11:09:51 +0200

    Fix annotation

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def wrapper(*args) -> None:
+    def wrapper(*args) -> Callable:
         if args in cached_data:
             print("Getting from cache")
         else:
