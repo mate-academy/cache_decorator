d00f7d9202d2b34e04df3eea01bee393292c3cf1
Atom-gnomov <gorbach0864213579@gmail.com>
2024-12-21 09:31:18 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_save = {}
+
+    def wrapper(*args, **kwargs) -> any:
+        cache_key = (args, tuple(sorted(kwargs.items())))
+        if cache_key in cache_save:
+            print("Getting from cache")
+            return cache_save[cache_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_save[cache_key] = result
+            return result
+    return wrapper
