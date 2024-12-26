f1b774a757f2fb26cae711322186b3e7860ae573
Dmytro <dima.sichkar2003@gmail.com>
2024-12-26 09:36:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,20 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        if any(isinstance(arg, (list, dict, set)) for arg in args):
+            raise ValueError("All arguments must be immutable")
+        key = (func, args, frozenset(kwargs.items()))
+
+        if key in cache_storage.keys():
+            print("Getting from cache")
+            return cache_storage[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
+            return result
+
+    return wrapper
