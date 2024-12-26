18b1370c93d61c52fcbad02361ac2fcba54242da
Leo <leonsiypopov@gmail.com>
2024-12-27 01:09:44 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from typing import Callable
 
+new_set = {}
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def wrapper(*args, **kwargs):
+        if (func.__name__, *args) in new_set:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            new_set[(func.__name__, *args)] = result
+            return result
+    return wrapper
+
