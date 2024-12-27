ed4c0ee5f9b10821f79a23a191653001650d7826
Leo <leonsiypopov@gmail.com>
2024-12-27 20:30:48 +0200

    gone 4

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    new_dict = {}
+    cache_dict = {}
 
     def wrapper(*args, **kwargs) -> Callable:
-        if (func.__name__, *args, str(**kwargs)) in new_dict:
+        if (args, str(kwargs)) in cache_dict:
             print("Getting from cache")
-            return new_dict[(func.__name__, *args, str(**kwargs))]
+            return cache_dict[(args, str(kwargs))]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
-            new_dict[(func.__name__, *args, str(**kwargs))] = result
+            cache_dict[(args, str(kwargs))] = result
             return result
     return wrapper
