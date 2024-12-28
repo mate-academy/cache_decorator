ec9ef0b1cbba0f4cce86dced7fb9e1db7a4f6f83
Nica <veronichkapapizh@gmail.com>
2024-12-28 13:22:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,23 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        try:
+            key = (args, frozenset(kwargs.items()))  # Незмінний ключ
+        except TypeError:
+            raise TypeError("Arguments must be immutable")
+
+        # Якщо ключ вже в кеші
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+
+        # Якщо ключа немає, обчислюємо результат
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        return result
+
+    return wrapper
