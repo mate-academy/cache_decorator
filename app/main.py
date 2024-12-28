ac4521da48c823752d31a9f91a5fd1823131058d
Ivan Khomych <ivan.khomychh@gmail.com>
2024-12-28 15:33:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_history = {}
+
+    def wrapper(*args):
+        nonlocal cache_history
+
+        if args in cache_history.keys():
+            print("Getting from cache")
+            return cache_history[args]
+        else:
+            print("Calculating new result")
+            result = func(args)
+            cache_history["args"] = result
+            return result
+    return wrapper
