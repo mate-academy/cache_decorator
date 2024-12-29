bb5bc37031d3777eb835980627d669e2092a1d38
Ivan Khomych <ivan.khomychh@gmail.com>
2024-12-29 15:23:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,14 +7,14 @@ def cache(func: Callable) -> Callable:
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
-        cache_key = args
+        cache_key = (args, tuple(kwargs.items()))
 
         if cache_key in cache_history:
             print("Getting from cache")
             return cache_history[cache_key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_history[cache_key] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_history[cache_key] = result
+        return result
     return wrapper
