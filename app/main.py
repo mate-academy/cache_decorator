2908fd2529256990835eacdf96a91712f8eb2fc0
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-01-03 19:14:57 +0200

    feat(cache): implement caching decorator with test support

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_store = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, tuple(kwargs.items()))
+        if key in cash_store:
+            print("Getting from cache")
+            return cash_store[key]
+        else:
+            result = func(*args, **kwargs)
+            cash_store[key] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
