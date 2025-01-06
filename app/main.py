8ba7f7bf98139b7fb316f52199b27e9bb47f35a5
Zikkolion <nmadaraosnova835@gmail.com>
2025-01-06 15:57:34 +0200

    Solution update

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
+    @wraps(func)
     def inner(*args, **kwargs) -> Any:
-        func_name = f"{func.__name__}"
+        func_name = func.__name__
+        key_name = (args, frozenset(kwargs.values()))
 
         if func_name not in cache_dict:
             cache_dict[func_name] = {}
-        if args not in cache_dict[func_name]:
-            cache_dict[func_name][args] = func(*args, **kwargs)
+        if key_name not in cache_dict[func_name]:
+            cache_dict[func_name][key_name] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
 
-        return cache_dict[func_name][args]
+        return cache_dict[func_name][key_name]
     return inner
