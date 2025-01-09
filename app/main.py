297f0f071fbdd91e9f1a5aa0209b007ca4c63007
bogdAAAn1 <privetpokaxaxaxa@gmail.com>
2025-01-09 12:33:15 +0200

    Fix annotation in wrapper function

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def wrapper(*args) -> Callable:
+    def wrapper(*args) -> Any:
         if args in cached_data:
             print("Getting from cache")
         else:
