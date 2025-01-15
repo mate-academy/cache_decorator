f4a4645d8be3a1ecba7dea9fe97e936f918cc52a
oh <wish.master.mvp@gmail.com>
2025-01-16 01:17:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_data = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in stored_data:
+            print("Getting from cache")
+            return stored_data.get(key)
+        else:
+            result = func(*args, **kwargs)
+            stored_data[key] = result
+            print("Calculating new result")
+            return result
+    return inner
