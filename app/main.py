a0fd5f1f3cf995b933d0549f52230142ac6f3e4c
Yurii <yakymenko025@gmail.com>
2025-01-15 14:20:36 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Dict, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    func_cache: Dict[Tuple, ] = {}  # Словник для зберігання результатів
+
+    def wrapper(*args) -> str:
+        if args in func_cache:
+            print("Getting from cache")
+            return func_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            func_cache[args] = result
+            return result
+
+    return wrapper
