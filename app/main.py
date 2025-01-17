b0856a4b54aba132fbdd7521e988eaf1e7b4f940
Liliia Plotnikova <liliiaplotnikova@MacBook-Air-Marcin.local>
2025-01-17 02:13:56 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    dict_funcs = {}
+
+    def inner(*args) -> Callable:
+        str_func = " ".join(str(val) for val in args)
+        if str_func in dict_funcs.keys():
+            print("Getting from cache")
+        else:
+            dict_funcs.update({str_func : func(*args)})
+            print("Calculating new result")
+
+        return dict_funcs[str_func]
+    return inner
