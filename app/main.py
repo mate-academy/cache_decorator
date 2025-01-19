3e63edd90d60e0ca1fa67407675e358584020374
Liliia Plotnikova <liliiaplotnikova@MacBook-Air-Marcin.local>
2025-01-19 02:02:03 +0100

    corrected solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from typing import Callable
 
 
-def cache(func: Callable, **dict_funcs) -> Callable:
-    def inner(*args, **dict_of_element) -> Callable:
+def cache(func: Callable) -> Callable:
+    def inner(*args) -> Callable:
+        dict_funcs = {}
         str_func = " ".join(str(val) for val in args)
-        if str_func in dict_funcs.keys():
+        if str_func in func.__dict__.keys():
             print("Getting from cache")
-            return dict_funcs[str_func]
+            return func.__dict__[str_func]
         else:
-            dict_of_element = {str_func : func(*args)}
-            dict_funcs.update(dict_of_element)
             print("Calculating new result")
-            return dict_of_element[str_func]
+            dict_funcs.update({str_func : func(*args)})
+            func.__dict__.update(dict_funcs)
+            return dict_funcs[str_func]
     return inner
