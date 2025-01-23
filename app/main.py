0ab3edfb092cf5bd120c127e599a86b5abc5c9c3
kbatrakov <kirillbatrakov0819@gmail.com>
2025-01-23 16:29:24 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-from typing import Callable
+from typing import Callable, Any
 
-
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable[[str, tuple, int, float, bool], Callable]):
+    operations_cache = []
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        if res not in operations_cache:
+            operations_cache.append(res)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return operations_cache[operations_cache.index(res)]
+    return wrapper
