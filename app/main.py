df79434f80f1ae274c8573196c4160b17a13f3d1
Vlad Bukin <vbukinnn2009@gmail.com>
2025-01-25 13:50:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-from typing import Callable
+from typing import Callable, List, Any
+import functools
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
+    cache_dict = {}
+
+    @functools.wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict) -> Any:
+        key = (args, tuple(kwargs.items()))
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        return result
+    return wrapper
+
+
+@cache
+def long_time_func(base: int, exponent: int, mod: int) -> int:
+    return (base ** exponent ** mod) % (base * mod)
+
+
+@cache
+def long_time_func_2(numbers: tuple, power: int) -> List[int]:
+    return [number ** power for number in numbers]
