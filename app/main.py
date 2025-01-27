d6b3784d5896ed8dc4061e66d9b532d5c9f1d51b
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-01-27 11:56:06 +0200

    Add cache decorator with support for caching results of functions with immutable arguments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
-from typing import Callable
+from idlelib.editor import keynames
+from typing import Callable, Any
+from functools import wraps
 
-
+function_caches = {}
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+   function_caches[func] = {}
+
+   @wraps(func)
+   def wrapper(*args, **kwargs) -> Any:
+
+     key = (args, tuple(sorted(kwargs.items())))
+
+     if key in function_caches[func]:
+        print("Getting from cache")
+        return function_caches[func][key]
+
+     print("Calculating new result")
+     result = func(*args, **kwargs)
+
+     function_caches[func][key] = result
+     return result
+
+
+   return wrapper
