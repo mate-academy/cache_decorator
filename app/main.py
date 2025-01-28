10c685c92aa89fc5fc7b1698bafdfbaa8f1f0073
Kiril Buntiv <kurulobuntov@gmail.com>
2025-01-29 01:53:51 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,22 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cashe_result ={}
+    cache_result ={}
     def func_results(*args, **kwargs):
         for arg in args:
             if not isinstance(arg,(int,str,float, tuple)):
-                print("Value is muttable")
+                print("Value is mutable")
                 return
         for keys, value in kwargs.items():
             if not isinstance(value,(int,str,float, tuple)):
-                print("Value is muttable")
+                print("Value is mutable")
                 return
         names = (args, tuple(kwargs.items()))
-        if names in cashe_result:
+        if names in cache_result:
             print("Getting from cache")
-            return cashe_result[names]
+            return cache_result[names]
         print("Calculating new result")
         res = func(*args, **kwargs)
-        cashe_result[names] = res
+        cache_result[names] = res
         return res
     return func_results
