ab1f3898fd82036d1416d90dca0a9fd02dea5ac1
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-02-01 18:31:35 +0200

    fix type annotation

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dic = {}
 
-    def wrapper(*args) -> None:
+    def wrapper(*args):
         if args in cache_dic:
             print("Getting from cache")
             return cache_dic[args]
