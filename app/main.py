9b26ef09bd1de9c25e551dd558286f742f0e25f5
Nikita-pyth <nicksan2002@gmail.com>
2025-02-02 12:49:27 +0200

    addresses the issues

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ def cache(func: Callable) -> Callable:
     cached_values = {}
 
     def wrapper(*args) -> int:
-        if args in cached_values.keys():
+        if args in cached_values:
             print("Getting from cache")
-            return cached_values[args]
         else:
             print("Calculating new result")
             result = func(*args)
             cached_values[args] = result
-            return result
+        return cached_values[args]
 
     return wrapper
