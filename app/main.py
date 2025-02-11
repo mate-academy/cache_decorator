c5a5a45a1b7630b2d4897de3391d7c3475680c5f
Marek Drządzewski <dzadzumaster@gmail.com>
2025-02-11 21:05:32 +0100

    Adding corrections according to flake8

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 from typing import Callable
 
 
-
 def cache(func: Callable) -> Callable:
     used_functions = {}
-    def wrapper(*args, **kwargs):
+
+    def wrapper(*args, **kwargs) -> Callable:
+
         key = (func.__name__, args, tuple(kwargs.items()))
 
         if key in used_functions:
             result = used_functions[key]
             print("Getting from cache")
         else:
-            used_functions[key] = func(*args,**kwargs)
+            used_functions[key] = func(*args, **kwargs)
             result = used_functions[key]
             print("Calculating new result")
         return result
     return wrapper
-
