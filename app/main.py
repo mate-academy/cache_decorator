a78e440973f42fd868b7dea59f699ad78e8408db
Illia <krabbys1111@gmail.com>
2025-02-11 23:48:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import functools
 
 
 def cache(func: Callable) -> Callable:
-    cache_resullt = {}  # Словник для збереження кешу
+    cache_resullt = {}
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -11,11 +11,11 @@ def cache(func: Callable) -> Callable:
 
         if key in cache:
             print("Getting from cache")
-            return cache_resullt[key]  # Повертаємо кешований результат
+            return cache_resullt[key]
 
         print("Calculating new result")
-        result = func(*args, **kwargs)  # Обчислюємо новий результат
-        cache_resullt[key] = result  # Зберігаємо в кеш
+        result = func(*args, **kwargs)
+        cache_resullt[key] = result
         return result
 
-    return wrapper  # Повертаємо обгорнуту функцію
+    return wrapper
