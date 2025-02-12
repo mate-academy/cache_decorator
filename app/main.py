b8c70d915e9d2b650191ed9b55397cc241356d6d
Yaroslav Zhuravlov <zyzy92187@gmail.com>
2025-02-12 15:40:31 +0200

    implement cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_memory = {}
+
+    def cache_wrapper(*args: tuple) -> any:
+        if (cached := cache_memory.get(args)) is not None:
+            print("Getting from cache")
+            return cached
+
+        print("Calculating new result")
+        cache_memory[args] = func(*args)
+
+        return cache_memory[args]
+
+    return cache_wrapper
