546e3630ace81d70170b9de538e3071b702b6034
Bohdan Mykyta <justkexitmail@gmail.com>
2025-02-12 23:58:41 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_direct = {}
+
+    def inner(*args) -> Any:
+        if args in cache_direct:
+            print("Getting from cache")
+            return cache_direct[args]
+        else:
+            print("Calculating new result")
+            res = func(*args)
+            cache_direct[args] = res
+            return res
+
+    return inner
