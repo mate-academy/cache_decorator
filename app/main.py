bdb7fc687571f3dc6baa0573b69ea84ed358e3a0
unknown <alex594.ao@gmail.com>
2025-02-12 19:38:11 -0800

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
-from typing import Callable
+from typing import Callable, Dict, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    """Caches results of function calls with immutable arguments."""
+    cache_store: Dict[Tuple, any] = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        cache_key = (args, tuple(sorted(kwargs.items())))
+
+        if cache_key in cache_store:
+            print("Getting from cache")
+            return cache_store[cache_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_store[cache_key] = result
+            return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(aa: int, bb: int, cc: int) -> int:
+    return (aa ** bb ** cc) % (aa * cc)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
