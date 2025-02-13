520bf2dc93b56b67543d39f41d165d7ef39398a5
Artem Riabchynskyi <ryabchinskyam@gmail.com>
2025-02-13 15:16:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-from typing import Callable
+from typing import Callable, Any, Hashable
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
+    cache_storage = {}
+
+    def wrapper(*args: Hashable, **kwargs: Hashable) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in cache_storage:
+            print("Getting from cache")
+            return cache_storage[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
+            return result  # Повертаємо результат!
+
+    return wrapper  # Повертаємо функцію wrapper
+
+
+@cache
+def long_time_func(num1: int, num2: int, num3: int) -> int:
+    return (num1 ** num2 ** num3) % (num1 * num3)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
