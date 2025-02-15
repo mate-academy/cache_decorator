41ccbc6cc79d12ebef87049c1b2b20d59e840ca0
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-02-15 15:03:39 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store = {}
+
+    def inner(*args: list) -> Callable:
+        key = hash(args)
+
+        if key in store:
+            print("Getting from cache")
+
+            return store[key]
+
+        print("Calculating new result")
+
+        res = func(*args)
+        store[key] = res
+
+        return res
+
+    return inner
+
