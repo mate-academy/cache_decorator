4a57ea076ff245f8733af492dbf74d9432b7c489
Yurii <stulkiv@gmail.com>
2025-02-15 04:15:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+def cache(func):
+    cache_data = {}
 
+    def wrapper(*args, **kwargs):
+        key = (func.__name__, args, tuple(kwargs.items()))
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+        if key not in cache_data:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_data[key] = result
+            return result
+
+        print("Getting from cache")
+        return cache_data[key]
+
+    return wrapper
