021b331eb89f917e2048591d20c4b661bd82696a
Myroslav Vorobei <miroslavcomputer@gmail.com>
2025-02-16 19:35:46 +0200

    Fixed variable name

--- app/main.py
+++ app/main.py
@@ -2,20 +2,20 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cache_storange = {}
+    cache_storage = {}
 
     def inner(*args, **kwargs):
-        if func not in cache_storange:
-            cache_storange[func] = {}
+        if func not in cache_storage:
+            cache_storage[func] = {}
 
         key = (args, tuple(sorted(kwargs.items())))
-        if key in cache_storange[func]:
+        if key in cache_storage[func]:
             print("Getting from cache")
-            return cache_storange[func][key]
+            return cache_storage[func][key]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
-            cache_storange[func][key] = result
+            cache_storage[func][key] = result
             return result
 
     return inner
