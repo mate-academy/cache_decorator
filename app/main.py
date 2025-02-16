05c5e4be0e14dd1fb3aa31fd9995fb8e5d77fb12
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-02-16 19:59:39 +0200

    implement py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
+
+
+def is_immutable(obj: Any) -> bool:
+    return isinstance(obj, (int, float, str, tuple, bool, type(None)))
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper_cache(*args: Any, **kwargs: Any) -> Any:
+        if (
+                not all(is_immutable(arg) for arg in args)
+                or not all(is_immutable(v) for v in kwargs.values())
+        ):
+            return func(*args, **kwargs)
+
+        cache_key = (args, tuple(sorted(kwargs.items())) if kwargs else ())
+
+        if cache_key not in wrapper_cache.cache:
+            print("Calculating new result")
+            wrapper_cache.cache[cache_key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+
+        return wrapper_cache.cache[cache_key]
+
+    wrapper_cache.cache = {}
+    return wrapper_cache
