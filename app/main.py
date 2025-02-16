c53520d1edfc0dd41c77e6321b0826ca264e96d8
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-02-16 11:22:19 +0200

    'Cache_decorator'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-from typing import Callable
+from typing import Callable, Dict, Tuple, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_results: Dict[Tuple[Any, ...], Any] = {}
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in stored_results:
+            print("Getting from cache")
+            return stored_results[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        stored_results[key] = result
+        return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(base: int, exponent: int, modulus: int) -> int:
+    return (base ** exponent ** modulus) % (base * modulus)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[int]:
+    return [number ** power for number in n_tuple]
