558b72cadce65a532668fe7e2ce57b616fc7b990
Yura Hryn <hrun.yuri@gmail.com>
2025-02-16 21:23:47 +0200

    Flake error correction

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
-    def wrapper(*args):
+
+    def wrapper(*args) -> Callable:
         if args not in cache_dict:
             print("Calculating new result")
             cache_dict[args] = func(*args)
