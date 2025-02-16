461d2ed9cc2df70ed304edfa613965ff350e0370
Piotr Żmudziński <piotrek125stunt@gmail.com>
2025-02-16 12:27:18 +0100

    '0Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def wrapper(*args):
+        completed_runs = []
+        result = func(completed_runs)
+        for value in (args):
+            if value is not isinstance(value, (int, float, str, bool, tuple,)):
+                pass
+            else:
+                completed_runs.append(result)
+
+
+            if value in ( args):
+                return completed_runs
+                print("Getting from cache")
+            else:
+                return func(completed_runs)
+                print("Calculating new result")
+
+        return func(args, kwargs)
+    return wrapper
