3f920f700b2f6c68679c8c97700c0f926645e35d
Piotr Żmudziński <piotrek125stunt@gmail.com>
2025-02-16 18:53:07 +0100

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
+    completed_runs = {}
     def wrapper(*args):
-        completed_runs = {}
+
         if args in completed_runs:
             print("Getting from cache")
             return completed_runs[args]
         else:
             print("Calculating new result")
             completed_runs[args] = func(*args)
+
         return completed_runs[args]
 
     return wrapper
