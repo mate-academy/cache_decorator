db9c11f525264c85ce9269fb086cde047ee0047f
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-02-17 15:40:05 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    data_cache = {}
+
+    @wraps(func)
+    def inner(*args) -> None:
+        if args in data_cache:
+            print("Getting from catche")
+            return data_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            data_cache[args] = result
+            return result
+
+    return inner
