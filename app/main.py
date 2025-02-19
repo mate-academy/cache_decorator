a65e5067ac1df75749916223c804d9539dfa0f8a
Max <smolinskijmaksim1@gmail.com>
2025-02-19 17:37:43 +0100

    fix flake

--- app/main.py
+++ app/main.py
@@ -7,33 +7,15 @@ def cache(func: Callable) -> Callable:
 
     @functools.wraps(func)
     def inner(*args: Any, **kwargs: Any) -> Any:
-        key_tuple = (args, frozenset(kwargs.items()))
+        key = (args, frozenset(kwargs.items()))
 
-        if key_tuple in cached_result:
+        if key in cached_result:
             print("Getting from cache")
-            return cached_result[key_tuple]
+            return cached_result[key]
 
         print("Calculating new result")
         result = func(*args, **kwargs)
-        cached_result[key_tuple] = result
+        cached_result[key] = result
+
         return result
-
     return inner
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
