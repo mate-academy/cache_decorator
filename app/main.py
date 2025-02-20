0f3aa1a5275b2d3e38550a9c8b06ba0b05eb785a
Maksym Omelkov <omelkov85@gmail.com>
2025-02-21 00:58:38 +0200

    implementing cash decorator (corrected 1.1)

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import Callable, Any
+import functools
 
 cash_dict = {}
 
 
 def cache(func: Callable) -> Callable:
+    @functools.wraps(func)
     def inner(*args) -> Any:
-        if id(func) not in cash_dict:
-            cash_dict[id(func)] = {}
-        if args not in cash_dict[id(func)]:
-            cash_dict[id(func)][args] = func(*args)
+        if func not in cash_dict:
+            cash_dict[func] = {}
+        if args not in cash_dict[func]:
+            cash_dict[func][args] = func(*args)
             print("Calculating new result")
-            return cash_dict[id(func)][args]
+            return cash_dict[func][args]
         else:
             print("Getting from cache")
-            return cash_dict[id(func)][args]
+            return cash_dict[func][args]
     return inner
