cd2bdd72a9149177ba98f11900e302c533e73b08
MishaLehotskyi <eglet777@ukr.net>
2025-02-24 17:52:18 +0200

    added task solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store = {}
+
+    def inner(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in store:
+            print("Getting from cache")
+            return store[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        store[key] = result
+        return result
+
+    return inner
