91a4f0addeab986110a98d30c7aa0218785837ee
ViktorBl734 <harbourprogress@gmail.com>
2025-02-26 16:27:57 +0200

    Making the Cach decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    caches = {}
+
+    def wraper(*args, **kwargs) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in caches:
+            print("Getting from cache")
+            return caches[key]
+        else:
+
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            caches[key] = result
+            return result
+    return wraper
