a0b7dd36c84c202b205314fe7085e9b8aca79e19
misha-cw <bigun.mykhaylo@gmail.com>
2025-02-27 20:59:47 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def cache(func: Callable) -> Callable:
     store_cache = {}
 
     def wrapper(*args) -> int:
-        if args in store_cache:
+        if args not in store_cache:
+            store_cache[args] = func(*args)
+            print("Calculating new result")
+        else:
             print("Getting from cache")
-            return store_cache[args]
-        store_cache[args] = func(*args)
-        print("Calculating new result")
         return store_cache[args]
 
     return wrapper
